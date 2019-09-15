e8821857c90e5443920f099c692468882016b59d
monitor <tracer@njuics.org>
2019-09-15 23:45:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -5,9 +5,21 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_add(src, dest, data_size);
 #else
+	/*9.15xiugai
 	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	assert(0);
 	return 0;
+	*/
+	uint32_t res =0;
+	res=dest+src;
+
+	set_CF_add(res,src,data_size);
+	set_PF(res);
+	set_ZF(res,data_size);
+	set_SF(res,data_size);
+	set_OF_add(res,src,dest,data_size);
+
+	return res&(0xFFFFFFFF>>(32-data_size));
 #endif
 }
 
