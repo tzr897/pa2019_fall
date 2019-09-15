c6b91c305e6a8c673778118817441d0366c8e530
monitor <tracer@njuics.org>
2019-09-16 00:32:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -242,3 +242,30 @@ void set_PF(uint32_t result)
 
 }
 
+void set_OF_add(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
+{
+	switch(data_size)
+	{
+		case 8:
+			result=sign_ext(result&0xFF,8);
+			src=sign_ext(src&0xFF,8);
+			dest=sign_ext(dest&0xFF,8);
+			break;
+		case 16:
+			result=sign_ext(result&0xFFFF,16);
+			src=sign_ext(src&0xFFFF,16);
+			dest=sign_ext(dest&0xFFFF,16);
+		default:break;
+	}
+	if(sign(src)==sign(dest))
+	{
+		if(sign(src)!=sign(result))
+			cpu.eflags.OF=1;
+		else
+			cpu.eflags.OF=0;
+	}
+	else
+	{
+		cpu.eflags.OF=0;
+	}
+}
