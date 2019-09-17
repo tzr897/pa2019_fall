5fac8ed59b0da05cf3a57d5d5808d2c71f4361c7
monitor <tracer@njuics.org>
2019-09-18 00:41:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -58,7 +58,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 	set_ZF(res,data_size);
 	set_SF(res,data_size);
 	set_OF_adc(res,src,dest,data_size);
-	
+	return res&(0xFFFFFFFF>>(32-data_size));
 	
 	
 #endif
@@ -296,7 +296,14 @@ void set_OF_add(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 	}
 }
 
-//alu_adc()/////////////////
+//alu_adc()////////////////
+
+uint32_t set_CF_adc(uint32_t result,uint32_t src,size_t data_size)
+{
+
+
+}
+
 void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 {
 	switch(data_size)
@@ -312,7 +319,17 @@ void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 			dest=sign_ext(dest&0xFFFF,16);
 		default:break;
 	}
-	
+	if(sign(src)==sign(dest))
+	{
+		if(sign(src)!=sign(result))
+			cpu.eflags.OF=1;
+		else
+			cpu.eflags.OF=0;
+	}
+	else
+	{
+		cpu.eflags.OF=0;
+	}
 
 
 
