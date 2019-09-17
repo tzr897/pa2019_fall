45b613f1f219f64e80d0fdcf18de03fb28b1681d
monitor <tracer@njuics.org>
2019-09-18 00:17:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -297,7 +297,28 @@ void set_OF_add(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 }
 
 //alu_adc()/////////////////
-void set_OF_adc()
+void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
+{
+	switch(data_size)
+	{
+		case 8:
+			result=sign_ext(result&0xFF,8);
+			src=sign_ext(src&0xFF,8);
+			dest=sign_ext(dest&0xFF,8);
+			break;
+		case 16:
+			result=sign_ext(result&0xFFFF,16);
+			src=sign_ext(src&0xFFFF,16);
+			dest=sign_ext(dest&0xFFFF,16);
+		default:break;
+	}
+	
+
+
+
+
+
+}
 
 
 
