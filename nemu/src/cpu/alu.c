871103b661571604de8c3299da19c4530531cf13
monitor <tracer@njuics.org>
2019-09-18 20:24:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -331,12 +331,12 @@ void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 			dest=sign_ext(dest&0xFFFF,16);
 		default:break;
 	}
-	if(result==0x7FFFFFFF+1)
+	/*if(result==0x7FFFFFFF+1)
 	{
 		cpu.eflags.OF=1;
 	}
 	else
-	{
+	{*/
 		if(sign(src)==sign(dest))
 		{
 			if(sign(src)!=sign(result))
@@ -348,7 +348,7 @@ void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 		{
 			cpu.eflags.OF=0;
 		}	
-	}
+	//}
 	
 
 }
