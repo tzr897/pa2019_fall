58e5219fe8daa458c5993305ae07d03f49b2cb3e
monitor <tracer@njuics.org>
2019-09-19 16:07:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -417,6 +417,34 @@ void set_OF_sub(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 
 //////////////////////////////////////////
 //alu_sbb()
+void set_CF_sbb(uint32_t result,uint32_t src,size_t data_size)
+{
+	result=sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
+	src=sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
+	if(cpu.eflags.CF==1)
+	{
+		if(result==src)
+		{
+			cpu.eflags.CF=1;
+		}
+		else
+		{
+			cpu.eflags.CF=src>(result+src);
+		}
+		
+	}
+	else
+	{
+		cpu.eflags.CF=src>(result+src);
+	}
 
+
+}
+
+void set_OF_sbb(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
+{
+
+
+}
 
 
