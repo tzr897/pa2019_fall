26cf223e8918924db3765de5828356ee072670b7
tracer-pa-public <tracer@njuics.org>
2019-09-19 14:26:59 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:26:59 up 53 min,  1 user,  load average: 0.64, 0.51, 0.34 d7afdc0ba1758d4722cddd2bff2156713c80d728

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -335,11 +335,24 @@ void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 			dest=sign_ext(dest&0xFFFF,16);
 		default:break;
 	}
+	/*
+	if(sign(src)==sign(dest))
+	{
+		if(sign(src)!=sign(result))
+			cpu.eflags.OF=1;
+		else
+			cpu.eflags.OF=0;
+	}
+	else
+	{
+		cpu.eflags.OF=0;
+	}
+	 */
 	if(cpu.eflags.CF==1)
 	{
 		if(result==src)
 		{
-			cpu.eflags.OF=1;
+			cpu.eflags.OF=0;
 		}
 		else
 		{
