1984cbfb3f7392c95af02e4850a1cb590d30ae6b
tracer-pa-public <tracer@njuics.org>
2019-09-19 15:24:35 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  15:24:35 up  1:32,  1 user,  load average: 1.03, 0.73, 0.50 2f45236320f88e7d4deefb8c8445599b7393113d

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -13,7 +13,7 @@ void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size);
 
 //alu_sub()
 void set_CF_sub(uint32_t result,uint32_t src,size_t data_size);
-
+void set_OF_sub(uint32_t result,uint32_t src,uint32_t dest,size_t data_size);
 
 
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
@@ -65,7 +65,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	set_PF(res);
 	set_ZF(res,data_size);
 	set_SF(res,data_size);
-	set_OF_add(res,src,dest,data_size);
+	set_OF_sub(res,src,dest,data_size);
 
 	return res&(0xFFFFFFFF>>(32-data_size));
 #endif
@@ -382,7 +382,34 @@ void set_CF_sub(uint32_t result,uint32_t src,size_t data_size)
 	cpu.eflags.CF=src>(result+src);
 }
 
+void set_OF_add(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
+{
+	switch(data_size)
+	{
+		case 8:
+			result=sign_ext(result&0xFF,8);
+			src=sign_ext(src&0xFF,8);
+			dest=sign_ext(dest&0xFF,8);
+			break;
+		case 16:
+			result=sign_ext(result&0xFFFF,16);
+			src=sign_ext(src&0xFFFF,16);
+			dest=sign_ext(dest&0xFFFF,16);
+		default:break;
+	}
+	if((sign(src)==sign(src+result))&&(sign(src)!=sign(result)))
+	{
+		cpu.eflags.OF=1;
+	}
+	else
+	{
+		cpu.flags.OF=0;
+	}
+	
 
+
+
+}
 
 
 
