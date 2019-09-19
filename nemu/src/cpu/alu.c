d19a2ac523d7c88386b07b1597e37eb4f8cfae63
tracer-pa-public <tracer@njuics.org>
2019-09-19 14:51:32 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:51:32 up 59 min,  1 user,  load average: 1.34, 0.89, 0.56 9ba771987172167c700b2482bf1e50be1221f80d

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -11,7 +11,8 @@ void set_OF_add(uint32_t result,uint32_t src,uint32_t dest,size_t data_size);
 void set_CF_adc(uint32_t result,uint32_t src,size_t data_size);
 void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size);
 
-
+//alu_sub()
+void set_CF_sub(uint32_t result,uint32_t src,size_t data_size);
 
 
 
@@ -73,7 +74,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	uint32_t res =0;
 	res=dest-src;
 
-	set_CF_add(res,src,data_size);
+	set_CF_sub(res,src,data_size);
 	set_PF(res);
 	set_ZF(res,data_size);
 	set_SF(res,data_size);
@@ -238,8 +239,8 @@ uint32_t alu_sal(uint32_t src, uint32_t dest, size_t data_size)
 	return 0;
 #endif
 }
-
-///////alu_add()////////////////
+////////////////////////////////////////////////////////
+//alu_add()
 //CF contains information relevant to unsigned integers
 void set_CF_add(uint32_t result,uint32_t src,size_t data_size)
 {
@@ -304,8 +305,8 @@ void set_OF_add(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 	}
 }
 
-//alu_adc()////////////////
-
+////////////////////////////////////////////////
+//alu_adc()
 void set_CF_adc(uint32_t result,uint32_t src,size_t data_size)
 {
 	result=sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
@@ -346,19 +347,6 @@ void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 			dest=sign_ext(dest&0xFFFF,16);
 		default:break;
 	}
-	/*
-	if(sign(src)==sign(dest))
-	{
-		if(sign(src)!=sign(result))
-			cpu.eflags.OF=1;
-		else
-			cpu.eflags.OF=0;
-	}
-	else
-	{
-		cpu.eflags.OF=0;
-	}
-	 */
 	if(cpu.eflags.CF==1)
 	{
 		if(result==src)
@@ -398,7 +386,14 @@ void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 	
 }
 
-
+/////////////////////////////////////////////////
+//set_CF_sub()
+void set_CF_sub(uint32_t result,uint32_t src,size_t data_size)
+{
+	result=sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
+	src=sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
+	cpu.eflags.CF=result>src;
+}
 
 
 
