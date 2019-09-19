f509e8102e1b4cbdc289203c4bc723c95dff0533
tracer-pa-public <tracer@njuics.org>
2019-09-19 14:24:47 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:24:47 up 50 min,  1 user,  load average: 0.63, 0.53, 0.33 a2c05db7fdc4c1b6f24f7330cabf1d565864381

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -299,35 +299,24 @@ void set_CF_adc(uint32_t result,uint32_t src,size_t data_size)
 {
 	result=sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
 	src=sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
-	// if(cpu.eflags.CF==1)
-	// {
-	// 	if(result==__INT_MAX__+__INT_MAX__+1)
-	// 		cpu.eflags.CF=1;
-	// 	else
-	// 	{
-	// 		cpu.eflags.CF=result<src;
-	// 	}
-		
-	// }
-	// else
-	// {
-	// 	cpu.eflags.CF=result<src;
-	// }
 	if(cpu.eflags.CF==1)
 	{
-		if(res==src)
+		if(result==src)
 		{
-			cpu.eflags.CF=0;
+			cpu.eflags.CF=1;
 		}
 		else
 		{
 			cpu.eflags.CF=result<src;
 		}
+		
 	}
 	else
 	{
 		cpu.eflags.CF=result<src;
 	}
+	
+	
 	
 }
 
@@ -348,16 +337,23 @@ void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 	}
 	if(cpu.eflags.CF==1)
 	{
-		if(sign(src)==sign(dest))
+		if(result==src)
 		{
-			if(sign(src)!=sign(result))
-				cpu.eflags.OF=1;
-			else
-				cpu.eflags.OF=1;
+			cpu.eflags.OF=1;
 		}
 		else
 		{
-			cpu.eflags.OF=0;
+			if(sign(src)==sign(dest))
+			{
+				if(sign(src)!=sign(result))
+					cpu.eflags.OF=1;
+				else
+					cpu.eflags.OF=0;
+			}
+			else
+			{
+				cpu.eflags.OF=0;
+			}
 		}
 	}
 	else
@@ -368,12 +364,13 @@ void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 				cpu.eflags.OF=1;
 			else
 				cpu.eflags.OF=0;
-		}
+		}	
 		else
 		{
 			cpu.eflags.OF=0;
 		}
 	}
+	
 	
 }
 
