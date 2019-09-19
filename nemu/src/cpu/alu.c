984f390963dd15f29bc092a4ca9253c53c038500
tracer-pa-public <tracer@njuics.org>
2019-09-19 15:38:35 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  15:38:35 up  1:46,  1 user,  load average: 0.31, 0.35, 0.44 5963d725c5de44635164c9dabba4aa66dd121c3d

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -65,7 +65,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	set_PF(res);
 	set_ZF(res,data_size);
 	set_SF(res,data_size);
-	set_OF_add(res,src,dest,data_size);
+	set_OF_sub(res,src,dest,data_size);
 
 	return res&(0xFFFFFFFF>>(32-data_size));
 #endif
@@ -397,9 +397,9 @@ void set_OF_sub(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 			dest=sign_ext(dest&0xFFFF,16);
 		default:break;
 	}
-	if(sign(dest)==sign(src))
+	if(sign(dest)!=sign(src))
 	{
-		if(sign(result)!=sign(src))
+		if(sign(result)==sign(src))
 			cpu.eflags.OF=1;
 		else
 			cpu.eflags.OF=0;	
