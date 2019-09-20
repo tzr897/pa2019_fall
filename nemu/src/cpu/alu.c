735eb0aa22136699f5ca324b925d08140b0ca01d
tracer-pa-public <tracer@njuics.org>
2019-09-20 14:21:52 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:21:52 up  3:53,  1 user,  load average: 0.08, 0.17, 0.12 df3cbdcd6327e3e2ee872942aa772d2c3326ebd9

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -425,7 +425,7 @@ void set_CF_sbb(uint32_t result,uint32_t src,size_t data_size)
 	src=sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
 	if(cpu.eflags.CF==1)
 	{
-		if(result==src)
+		if(result+src+1==result)
 		{
 			cpu.eflags.CF=1;
 		}
