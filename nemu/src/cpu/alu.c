7956c5acf5c50f840d3ad6fc7f44233225d933d6
tracer-pa-public <tracer@njuics.org>
2019-09-20 14:56:51 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:56:51 up  4:28,  1 user,  load average: 0.44, 0.34, 0.32 1a5ef32a3e487c65322552101a9155072caa986d

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -191,9 +191,13 @@ uint32_t alu_or(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_or(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	uint32_t res;
+	res=(src|dest);
+	res=res&(0xFFFFFFFF>>(32-data_size));
+	set_PF(res);
+	set_ZF(res,data_size);
+	set_SF(res,data_size);
+	return res;
 #endif
 }
 
