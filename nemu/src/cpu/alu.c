80e18dddfb8cad639543d5fb2f0a479061d631a2
tracer-pa-public <tracer@njuics.org>
2019-09-21 18:01:52 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  18:01:52 up 20 min,  1 user,  load average: 0.23, 0.21, 0.18 8ab0c25740d8d6e02c2d3eaa584c9dd4e2d81db6

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -268,7 +268,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 		res = dest >> src;
 		//res = res & (0xFFFFFFFF >> (32 - data_size));
 	}
-	else if(sign==1)
+	else
 	{
 		for (i = 0; i < src; ++i)
 		{
