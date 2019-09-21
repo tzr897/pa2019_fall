80935f1b95f90881a25230d12aef9455f4a70933
tracer-pa-public <tracer@njuics.org>
2019-09-21 14:08:38 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:08:38 up  3:32,  1 user,  load average: 0.10, 0.17, 0.16 9d2150f25e864102f3ed4ef8d945994f140f9821

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -229,31 +229,20 @@ uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_shr(src, dest, data_size);
 #else
-	// uint32_t res,t;
-	// bool sign;	
-	// //src=src&(0xFFFFFFFF>>(32-data_size));
-	// res=dest>>src;
-	// res=res&(0xFFFFFFFF>>(32-data_size));
+	// uint32_t res;
+	// bool sign;
+	// src=src&(0xFFFFFFFF>>(32-data_size));
+	// res=(unsigned int)dest>>(src-1);
 	// t=res;
 	// sign=t&0x1;
+	// res=t>>1;
+	// res=res&(0xFFFFFFFF>>(32-data_size));
 	// set_PF(res);
 	// set_ZF(res,data_size);
 	// set_SF(res,data_size);
 	// cpu.eflags.CF=sign;
 	// return res;
-	uint32_t res;
-	bool sign;
-	src=src&(0xFFFFFFFF>>(32-data_size));
-	res=(unsigned int)dest>>(src-1);
-	t=res;
-	sign=t&0x1;
-	res=t>>1;
-	res=res&(0xFFFFFFFF>>(32-data_size));
-	set_PF(res);
-	set_ZF(res,data_size);
-	set_SF(res,data_size);
-	cpu.eflags.CF=sign;
-	return res;
+	printf("%x\n",0x1010011>>2);
 #endif
 }
 
