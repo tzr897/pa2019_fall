9b7f65165b7c0659f8e23d9d1695a6af807f22e5
monitor <tracer@njuics.org>
2019-09-21 13:09:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -229,17 +229,20 @@ uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_shr(src, dest, data_size);
 #else
-	uint32_t res,t;
-	bool sign;	
-	src=src&(0xFFFFFFFF>>(32-data_size));
-	res=dest>>src;
-	res=res&(0xFFFFFFFF>>(32-data_size));
-	sign=res&0x1;
-	set_PF(res);
-	set_ZF(res,data_size);
-	set_SF(res,data_size);
-	cpu.eflags.CF=sign;
-	return res;
+	// uint32_t res;
+	// bool sign;	
+	// src=src&(0xFFFFFFFF>>(32-data_size));
+	// res=dest>>src;
+	// res=res&(0xFFFFFFFF>>(32-data_size));
+	// sign=res&0x1;
+	// set_PF(res);
+	// set_ZF(res,data_size);
+	// set_SF(res,data_size);
+	// cpu.eflags.CF=sign;
+	// return res;
+	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+	assert(0);
+	return 0;
 #endif
 }
 
