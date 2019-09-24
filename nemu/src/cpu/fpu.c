f5f06e44569a8fbc9bf301d4341e8c1632815583
tracer-pa-public <tracer@njuics.org>
2019-09-24 16:42:01 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  16:42:01 up  6:33,  1 user,  load average: 0.00, 0.06, 0.07 cf8fc176f960ca6c387ae9b959d641c51672d907

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -94,8 +94,8 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 	if (!overflow)
 	{
 		/* TODO: round up and remove the GRS bits */
-		bool sticky;
-		sticky=sig_grs&0x1;
+		//bool sticky;
+		//sticky=sig_grs&0x1;
 		//if(sticky)
 	}
 
