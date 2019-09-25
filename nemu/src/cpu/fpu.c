45cceffbad756ce4a1074b70d1ea4a60473d55b3
monitor <tracer@njuics.org>
2019-09-25 14:53:01 +0800

    git monitor commit.

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -109,6 +109,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 	else if (exp == 0 && sig_grs >> (23 + 3) == 1)//feiguigehuashu dedao guigehuashu
 	{
 		// two denormals result in a normal
+		sig_grs=(sig_grs>>1);
 		exp++;
 	}
 
