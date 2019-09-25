cd84fd76b9af92220ad44d0e531b304f693dc91b
monitor <tracer@njuics.org>
2019-09-25 14:27:01 +0800

    git monitor commit.

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -11,7 +11,9 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 
 	// normalization
 	bool overflow = false; // true if the result is INFINITY or 0 during normalize
-
+	//
+	uint32_t sticky=0;
+	//
 	if ((sig_grs >> (23 + 3)) > 1 || exp < 0)//xuyao yougui
 	{
 		// normalize toward right
@@ -25,7 +27,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			//sig_grs=sig_grs>>1;
 			exp++;
 			//sticky bit
-			uint32_t sticky = 0;
+			//uint32_t sticky = 0;
 			sticky = sticky | (sig_grs & 0x1);
 			sig_grs = sig_grs >> 1;
 			sig_grs |= sticky;
@@ -55,7 +57,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			
 			//sig_grs=sig_grs>>1;
 			//sticky bit
-			uint32_t sticky = 0;
+			//uint32_t sticky = 0;
 			sticky = sticky | (sig_grs & 0x1);
 			sig_grs = sig_grs >> 1;
 			sig_grs |= sticky;
@@ -92,7 +94,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 
 			//sig_grs=sig_grs>>1;
 			//sticky bit
-			uint32_t sticky = 0;
+			//uint32_t sticky = 0;
 			sticky = sticky | (sig_grs & 0x1);
 			sig_grs = sig_grs >> 1;
 			sig_grs |= sticky;
