995fb57e41ea92f530651ef662a77c4e41c8e31b
tracer-pa-public <tracer@njuics.org>
2019-09-25 19:19:20 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  19:19:20 up  4:39,  1 user,  load average: 0.34, 0.16, 0.17 a246aa1ad6578b0a231e839de48bffdcd75552c8

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -99,7 +99,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			sticky = sticky | (sig_grs & 0x1);
 			sig_grs = sig_grs >> 1;
 			sig_grs |= sticky;
-
+			exp++;
 		}
 	}
 	else if (exp == 0 && sig_grs >> (23 + 3) == 1)//feiguigehuashu dedao guigehuashu
