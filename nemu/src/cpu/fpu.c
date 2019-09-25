7f8fbe50e9bef100c935d72e3614a8d46037faab
tracer-pa-public <tracer@njuics.org>
2019-09-25 18:53:33 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  18:53:33 up  4:13,  1 user,  load average: 1.13, 0.81, 0.52 b32130b6467786bacc11e59defa6f8e3b49511c1

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -26,8 +26,6 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			/* TODO: shift right, pay attention to sticky bit*/
 			//sig_grs=sig_grs>>1;
 			exp++;
-			//sticky bit
-			//uint32_t sticky = 0;
 			sticky = sticky | (sig_grs & 0x1);
 			sig_grs = sig_grs >> 1;
 			sig_grs |= sticky;
@@ -135,7 +133,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		}
 
 		
-		if((sig_grs>>23)>1)
+		if((sig_grs>>23)>0)
 		{
 			sig_grs=(sig_grs>>1);
 			exp++;
