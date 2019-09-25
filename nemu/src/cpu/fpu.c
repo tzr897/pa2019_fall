554af0f8f0f3cc05993e21e5a7261e6ae0ff0e41
tracer-pa-public <tracer@njuics.org>
2019-09-25 17:51:47 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:51:47 up  3:43,  1 user,  load average: 0.40, 0.21, 0.14 46cdb4055b5d3e4fd060badc93ba217137a7daa3

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -140,7 +140,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		{
 			sig_grs=(sig_grs>>1);
 			exp++;
-			if(exp>=0xff)
+			if(exp==0xff)
 			{
 				if(sign==1)
 				{
@@ -154,22 +154,22 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			}
 
 		}
-		else if(((sig_grs>>23)==1)&&(sig_grs==0));
-		{
-			exp++;
-			if(exp>=0xff)
-			{
-				if(sign==1)
-				{
-					return N_INF_F;
-				}
-				else
-				{
-					return P_INF_F;
-				}			
-				overflow = true;
-			}
-		}
+		// else if(((sig_grs>>23)==1)&&(sig_grs==0));
+		// {
+		// 	exp++;
+		// 	if(exp>=0xff)
+		// 	{
+		// 		if(sign==1)
+		// 		{
+		// 			return N_INF_F;
+		// 		}
+		// 		else
+		// 		{
+		// 			return P_INF_F;
+		// 		}			
+		// 		overflow = true;
+		// 	}
+		// }
 
 		
 	}
