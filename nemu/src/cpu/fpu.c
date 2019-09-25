7ca5e0eea131154f63e290515edfa1bf590161ce
monitor <tracer@njuics.org>
2019-09-26 01:04:01 +0800

    git monitor commit.

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -352,7 +352,8 @@ uint32_t internal_float_mul(uint32_t b, uint32_t a)
 	uint32_t exp_res = 0;
 
 	/* TODO: exp_res = ? leave space for GRS bits. */
-	exp_res=fa.exponent+fb.exponent-22;
+	sig_res
+	exp_res=fa.exponent+fb.exponent-20;
 
 
 
