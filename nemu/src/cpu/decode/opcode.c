ecba950f52f342229cbb66bfef5bd03281314557
tracer-pa-public <tracer@njuics.org>
2019-10-16 20:42:47 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:42:47 up 11:19,  1 user,  load average: 0.65, 0.30, 0.19 56798d4ad289a35ea48b69c7cbd53fe6cec00fe

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -1,7 +1,7 @@
 #include "cpu/instr.h"
 
 instr_func opcode_entry[256] = {
-    /* 0x00 - 0x03*/ inv, mov_zrm162r_l, inv, inv,//10.16gai
+    /* 0x00 - 0x03*/ inv, mov_srm162r_l, inv, inv,//10.16gai?
     /* 0x04 - 0x07*/ inv, inv, inv, inv,
     /* 0x08 - 0x0b*/ inv, inv, inv, inv,
     /* 0x0c - 0x0f*/ inv, inv, inv, opcode_2_byte,
@@ -33,7 +33,7 @@ instr_func opcode_entry[256] = {
     /* 0x74 - 0x77*/ inv, inv, inv, inv,
     /* 0x78 - 0x7b*/ inv, inv, inv, inv,
     /* 0x7c - 0x7f*/ inv, inv, inv, inv,
-    /* 0x80 - 0x83*/ group_1_b, group_1_v, nemu_trap, cmps_v,//10.16gai
+    /* 0x80 - 0x83*/ group_1_b, group_1_v, nemu_trap, cmps_v,//10.16gai?
     /* 0x84 - 0x87*/ inv, inv, inv, inv,
     /* 0x88 - 0x8b*/ mov_r2rm_b, mov_r2rm_v, mov_rm2r_b, mov_rm2r_v,
     /* 0x8c - 0x8f*/ inv, inv, inv, inv,
