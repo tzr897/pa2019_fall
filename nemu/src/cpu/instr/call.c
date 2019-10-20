c4f4b1d2b38370f45d0bde0edfa7ebede3671c83
monitor <tracer@njuics.org>
2019-10-20 17:26:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -2,5 +2,14 @@
 
 make_instr_func(call_near)
 {
-    
+    OPERAND i1,i2;
+    i1.data_size=data_size;
+    i1.type=OPR_IMM;
+    i1.addr=eip+1;
+    i1.sreg=SREG_CS;
+    operand_read(&i1);
+
+    i2.data_size=
+
+
 }
