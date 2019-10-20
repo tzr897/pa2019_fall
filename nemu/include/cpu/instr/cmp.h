835de6bd6142841e234c0e9b58a4d3c9f31504d5
monitor <tracer@njuics.org>
2019-10-20 14:58:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -8,22 +8,8 @@ make_instr_func(cmp_rm2r_v)
 make_instr_func(cmp_i2rm_b)
 make_instr_func(cmp_i2rm_v)
 make_instr_func(cmp_i2rm_bv)
-make_instr_func(cmp_i2r_v)
-make_instr_func(cmp_2rm_bv)
+make_instr_func(cmp_i2a_b)
+make_instr_func(cmp_i2a_v)
 
-
-
-
-
-make_instr_impl_2op(cmp,r,rm,b)
-make_instr_impl_2op(cmp,r,rm,v)
-make_instr_impl_2op(cmp,rm,r,b)
-make_instr_impl_2op(cmp,rm,r,v)
-make_instr_impl_2op(cmp,i,rm,b)
-make_instr_impl_2op(cmp,i,rm,v)
-make_instr_impl_2op(cmp,i,rm,bv)
-
-make_instr_impl_2op(cmp, i, a, b)
-make_instr_impl_2op(cmp, i, a, v)
 
 #endif
