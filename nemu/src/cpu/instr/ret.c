b3c0eb01e6e7b8a79bb018b3cc115fbd9b1281b7
tracer-pa-public <tracer@njuics.org>
2019-10-21 20:49:47 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:49:47 up 1 day, 24 min,  1 user,  load average: 0.35, 0.56, 0.37 c37cec59cb4f0448e72a536d6a26d3b3bdb4e974

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -6,7 +6,7 @@ make_instr_func(ret_near)
     i.data_size=data_size;
     i.type=OPR_MEM;
     i.addr=cpu.esp;
-    i.sreg=SREG_SS;
+    i.sreg=SREG_DS;
     operand_read(&i);
     cpu.esp+=data_size/8;
     cpu.eip=i.val;
