a618c2d6a78a5b07177b3a8a67e0565483113921
tracer-pa-public <tracer@njuics.org>
2019-10-21 20:35:24 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:35:24 up 1 day, 9 min,  1 user,  load average: 0.42, 0.28, 0.16 f1252ec5d89b98e0559c0ce1eeab1b19ce980020

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -2,22 +2,22 @@
 
 make_instr_func(call_near)
 {
-    OPERAND i1,i2;
-    i1.data_size=data_size;
-    i1.type=OPR_IMM;
-    i1.addr=eip+1;
-    i1.sreg=SREG_CS;
-    operand_read(&i1);
+    OPERAND rel,i;
+    rel.data_size=data_size;
+    rel.type=OPR_IMM;
+    rel.addr=eip+1;
+    rel.sreg=SREG_CS;
+    operand_read(&rel);
 
-    i2.data_size=data_size;
-    i2.type=OPR_MEM;
+    i.data_size=data_size;
+    i.type=OPR_MEM;
     cpu.esp-=4;
-    i2.addr=cpu.esp;
-    i2.sreg=SREG_SS;
-    i2.val=(eip+1)+data_size/8;
-    operand_write(&i2);
+    i.addr=cpu.esp;
+    i.sreg=SREG_SS;
+    i.val=(eip+1)+data_size/8;
+    operand_write(&i);
 
-    cpu.eip+=sign_ext(i1.val,data_size)+1+data_size/8;
+    cpu.eip+=sign_ext(rel.val,data_size)+1+data_size/8;
     return 0;
 
 }
