a2bfe58528427e6d6b05cf575151ce63986cbdb2
tracer-pa-public <tracer@njuics.org>
2019-10-21 20:46:39 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:46:39 up 1 day, 21 min,  1 user,  load average: 0.81, 0.53, 0.31 18f002779f71787d823b3e33c7e28120974b2355

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -24,6 +24,8 @@ extern uint8_t data_size;
 #include "cpu/instr/cmp.h"
 #include "cpu/instr/push.h"
 #include "cpu/instr/call.h"
+#include "cpu/instr/ret.h"
+
 
 
 /* TODO: add more instructions here */
