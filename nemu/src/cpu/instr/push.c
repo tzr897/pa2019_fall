b2e12533121d1bf2364085521fca2a1724932597
monitor <tracer@njuics.org>
2019-10-24 14:50:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -5,7 +5,7 @@ static void instr_execute_1op()
     operand_read(&opr_src);
     cpu.esp-=data_size/8;
     //opr_dest.data_size=data_size/8;
-    opr_src.data_size=data_size/8;
+    //opr_src.data_size=data_size/8;
     opr_dest.type=OPR_MEM;
     opr_dest.addr=cpu.esp;
     opr_dest.sreg=SREG_DS;//
