9fa7055fc56a936770dadf66333a9fe7e740c895
tracer-pa-public <tracer@njuics.org>
2019-10-24 11:30:53 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  11:30:53 up 1 day,  6:12,  1 user,  load average: 0.30, 0.26, 0.14 b24dc696c1fc62f048c2f5dda7f49c6264598

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -1,6 +1,6 @@
 #include "cpu/instr.h"
 
-static void instr_execute_2op() 
+static void instr_execute_1op() 
 {
     operand_read(&opr_dest);
     opr_dest.val=sign_ext(opr_dest.val,opr_dest.data_size);
