605c1e10b345f138325b9cd1da6d2e317cacc3a8
tracer-pa-public <tracer@njuics.org>
2019-10-24 15:47:55 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  15:47:55 up 1 day,  9:07,  1 user,  load average: 0.19, 0.28, 0.34 a6932f491ab59e14dcca4898bff0750efc9dff3

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -6,10 +6,11 @@ make_instr_func(lea)
 	//OPERAND opr_dest;
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
+
 	opr_src.data_size=data_size;
 	opr_dest.data_size=data_size;
 
-	len += modrm_r_rm(cpu.eip+1, &opr_dest, &opr_src);
+	len += modrm_r_rm(cpu.eip+1, &opr_src, &opr_dest);
 
 	opr_dest.val = opr_src.addr;
 	
