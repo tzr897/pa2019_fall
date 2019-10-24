723ff3a83ad6b1efeb2bfabff608ff3107b308c3
monitor <tracer@njuics.org>
2019-10-24 11:28:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -12,19 +12,7 @@ static void instr_execute_2op()
 make_instr_impl_2op(inc, rm, b)
 make_instr_impl_2op(inc, rm, v)
 make_instr_impl_2op(inc, rm, bv)
+make_instr_impl_2op(inc, r, b)
+make_instr_impl_2op(inc, r, v)
+make_instr_impl_2op(inc, r, bv)
 
-
-
-
-make_instr_impl_2op(add, r, rm, v)
-make_instr_impl_2op(add, rm, r, b)
-make_instr_impl_2op(add, rm, r, v)
-make_instr_impl_2op(add, i, rm, b)
-make_instr_impl_2op(add, i, rm, v)
-make_instr_impl_2op(add, i, rm, bv)
-make_instr_impl_2op(add, i, r, b)
-make_instr_impl_2op(add, i, r, v)
-make_instr_impl_2op(add, a, o, b)
-make_instr_impl_2op(add, a, o, v)
-make_instr_impl_2op(add, i, a, b)
-make_instr_impl_2op(add, i, a, v)
