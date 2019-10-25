778ee6e8d0b692418d44a69e73b1099b2beba2be
monitor <tracer@njuics.org>
2019-10-25 18:26:01 +0800

    git monitor commit.

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -27,7 +27,7 @@ instr_func opcode_entry[256] = {
     /* 0x5c - 0x5f*/ inv, pop_r_v, inv, inv,//5d:pop_r_v
     /* 0x60 - 0x63*/ inv, inv, inv, inv,
     /* 0x64 - 0x67*/ inv, inv, data_size_16, inv,
-    /* 0x68 - 0x6b*/ inv, inv, push_i_v, inv,
+    /* 0x68 - 0x6b*/ inv, inv, push_i_b, inv,
     /* 0x6c - 0x6f*/ inv, inv, inv, inv,
     /* 0x70 - 0x73*/ inv, inv, inv, inv,//
     /* 0x74 - 0x77*/ je_short_, jne_short_, jna_short_, inv,//74je_short_,75:jne_short_,76:jna_short_
