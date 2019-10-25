7c157b4799e53500523431f24eb99845992fb39e
monitor <tracer@njuics.org>
2019-10-25 16:38:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -3,7 +3,6 @@
 make_instr_func(leave)
 {
     cpu.esp=cpu.ebp;
-    //operand_read(&opr_dest);
     
     OPERAND i;
     i.type=OPR_MEM;
@@ -17,12 +16,26 @@ make_instr_func(leave)
     return 1;
 }
 
-// opr_dest.type=OPR_MEM;
-//     opr_dest.addr=cpu.esp;
-//     opr_dest.sreg=SREG_DS;//
-//     operand_read(&opr_dest);
+/*
+#include "cpu/instr.h"
 
-//     opr_src.val=opr_dest.val;
+static void instr_execute_1op()
+{
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.data_size=data_size;
+    opr_dest.sreg=SREG_DS;//
+    operand_read(&opr_dest);
+
+    opr_src.val=opr_dest.val;
     
-//     operand_write(&opr_src);
-//     cpu.esp+=data_size/8;
+    operand_write(&opr_src);
+    cpu.esp+=data_size/8;
+    
+}
+
+make_instr_impl_1op(pop, rm, v)
+make_instr_impl_1op(pop, r, v)
+make_instr_impl_1op(pop, i, v)
+
+ */
