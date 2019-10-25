5c317b0d7ce6f70f52fa2f2d81b8e03775bded00
monitor <tracer@njuics.org>
2019-10-25 14:23:01 +0800

    git monitor commit.

---
+++ nemu/src/cpu/instr/pop.c
@@ -0,0 +1,19 @@
+#include "cpu/instr.h"
+
+static void instr_execute_1op()
+{
+    operand_read(&opr_src);
+
+    cpu.esp+=data_size/8;
+
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    
+    opr_dest.sreg=SREG_DS;//
+    opr_dest.val=opr_src.val;
+    operand_write(&opr_dest);
+}
+
+make_instr_impl_1op(pop, rm, v)
+make_instr_impl_1op(pop, r, v)
+make_instr_impl_1op(pop, i, v)
