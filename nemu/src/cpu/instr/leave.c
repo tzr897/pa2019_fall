1109d8ccde268fd06e0a9570e7d6318ec010a40c
tracer-pa-public <tracer@njuics.org>
2019-10-25 16:14:36 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  16:14:36 up 1 day, 12:25,  1 user,  load average: 0.28, 0.23, 0.18 4a6b90d7bb7e0e1994070921b0ca20b012c6a9ce

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -2,16 +2,17 @@
 
 make_instr_func(lea)
 {
-    int len = 1;
-	opr_src.data_size=data_size;
-	opr_dest.data_size=data_size;
+    // int len = 1;
+	// opr_src.data_size=data_size;
+	// opr_dest.data_size=data_size;
 
-	len += modrm_r_rm(cpu.eip+1, &opr_dest, &opr_src);
+	// len += modrm_r_rm(cpu.eip+1, &opr_dest, &opr_src);
 
-	opr_dest.val = opr_src.addr;
+	// opr_dest.val = opr_src.addr;
 	
-	operand_write(&opr_dest);
+	// operand_write(&opr_dest);
     cpu.esp=cpu.ebp;
+    pop_r_v(cpu.ebp);
 
 	return len;
 
