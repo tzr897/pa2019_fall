236513b78bb6fa04567bbc39ea9b5beb3ee0fc7e
tracer-pa-public <tracer@njuics.org>
2019-10-25 14:57:34 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:57:34 up 1 day, 11:14,  1 user,  load average: 0.40, 0.41, 0.35 f31701e0f93656199f464d76481324fb78edb13

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -3,15 +3,15 @@
 static void instr_execute_1op()
 {
     operand_read(&opr_dest);
-    
+
     opr_src.type=OPR_MEM;
     opr_src.addr=cpu.esp;
-    
+    cpu.esp+=data_size/8;
     opr_src.sreg=SREG_DS;//
     opr_src.val=opr_dest.val;
     operand_write(&opr_src);
 
-    cpu.esp+=data_size/8;
+    
 }
 
 make_instr_impl_1op(pop, rm, v)
