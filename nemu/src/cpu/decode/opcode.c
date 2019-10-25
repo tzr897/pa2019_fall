91e7230d03e66a4550d0e6ddce491d6efd54e99f
tracer-pa-public <tracer@njuics.org>
2019-10-25 16:59:40 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  16:59:40 up 1 day, 13:10,  1 user,  load average: 0.28, 0.31, 0.25 d7167548824205cb7a9689367df9a4c3c8a8e0f2

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -15,7 +15,7 @@ instr_func opcode_entry[256] = {
     /* 0x2c - 0x2f*/ inv, inv, inv, inv,
     /* 0x30 - 0x33*/ inv, xor_r2rm_v, inv, inv,//31:xor_r2rm_v
     /* 0x34 - 0x37*/ inv, inv, inv, inv,
-    /* 0x38 - 0x3b*/ inv, cmp_r2rm_v, inv, inv,//39:cmp_r2rm_v
+    /* 0x38 - 0x3b*/ inv, cmp_r2rm_v, inv, cmp_rm2r_v,//39:cmp_r2rm_v
     /* 0x3c - 0x3f*/ inv, inv, inv, inv,
     /* 0x40 - 0x43*/ inv, inv, inc_r_v, inv,//42:inc_r_v
     /* 0x44 - 0x47*/ inv, inv, inv, inv,
@@ -63,7 +63,7 @@ instr_func opcode_entry[256] = {
     /* 0xec - 0xef*/ inv, inv, inv, inv,
     /* 0xf0 - 0xf3*/ inv, break_point, inv, rep_repe,
     /* 0xf4 - 0xf7*/ hlt, inv, group_3_b, group_3_v,//group_3_v,10.17f7group_3_v->cmps_v,f6group_3_b->cmps_v,f5cmps_v
-    /* 0xf8 - 0xfb*/ clc, inv, cmp_rm2r_v, inv,//10.17fbcmps_v,facmps_V,f9cmps_v
+    /* 0xf8 - 0xfb*/ clc, inv, inv, inv,//10.17fbcmps_v,facmps_V,f9cmps_v
     /* 0xfc - 0xff*/ cld, inv, inv, group_5_indirect,//10.17ffcmps_v,fecmps_v,fdcmps_v
 };
 
