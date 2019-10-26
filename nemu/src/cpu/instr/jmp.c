c39aa71acf89b9897cd0a07e3bc34845424f1656
tracer-pa-public <tracer@njuics.org>
2019-10-26 20:52:31 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:52:31 up 1 day, 23:22,  1 user,  load average: 0.32, 0.13, 0.09 7bb647dbb33600fa66f49ce93f94faafb5a3a83f

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -62,11 +62,12 @@ make_instr_func(jmp_near_indirect)
 
         operand_read(&rel);
 
-        rel.val = sign_ext(rel.val, data_size);
+        
         // thank Ting Xu from CS'17 for finding this bug
         //print_asm_1("jmp", "", 1 + data_size / 8, &rel);
         if(data_size==16)
         {
+                rel.val = sign_ext(rel.val, data_size);
                 cpu.eip=rel.val&0xffff;
         }
         else if(data_size==32)
