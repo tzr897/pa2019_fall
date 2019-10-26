c8a3a49069ffe4656c1e5bcb11590bf5369b6edc
tracer-pa-public <tracer@njuics.org>
2019-10-27 01:21:52 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  01:21:52 up 2 days,  2:27,  1 user,  load average: 0.50, 0.44, 0.34 20fe9d7a773281a6ca61e72463cd218f90c8b815

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -183,10 +183,10 @@ instr_func opcode_2_byte_entry[256] = {
     /* 0x74 - 0x77*/ inv, inv, inv, inv,
     /* 0x78 - 0x7b*/ inv, inv, inv, inv,
     /* 0x7c - 0x7f*/ inv, inv, inv, inv,
-    /* 0x80 - 0x83*/ inv, inv, inv, jae_near,
-    /* 0x84 - 0x87*/ je_near, jne_near, jna_near, ja_near,//86:jna_near,84:je_near,87:ja_near
-    /* 0x88 - 0x8b*/ js_near, jns_near, inv, inv,
-    /* 0x8c - 0x8f*/ jl_near, inv, jle_near, jg_near,//8c:jl_near,8e:jle_near
+    /* 0x80 - 0x83*/ __ref_jo_near, __ref_jno_near, __ref_jb_near, __ref_jae_near,
+    /* 0x84 - 0x87*/ __ref_je_near, __ref_jne_near, __ref_jna_near, __ref_ja_near,//86:jna_near,84:je_near,87:ja_near
+    /* 0x88 - 0x8b*/ __ref_js_near, __ref_jns_near, __ref_jp_near, __ref_jnp_near,
+    /* 0x8c - 0x8f*/ __ref_jl_near, __ref_jge_near, __ref_jle_near, __ref_jg_near,//8c:jl_near,8e:jle_near
     /* 0x90 - 0x93*/ inv, inv, inv, inv,
     /* 0x94 - 0x97*/ inv, setne_b, inv, inv,//95:setne_b
     /* 0x98 - 0x9b*/ inv, inv, inv, inv,
