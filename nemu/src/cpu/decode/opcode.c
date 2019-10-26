b77b9553e3bb6b1ceacce500fa5dc516fee07f15
tracer-pa-public <tracer@njuics.org>
2019-10-27 01:01:25 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  01:01:25 up 2 days,  2:07,  1 user,  load average: 0.23, 0.22, 0.28 34dfaf8ebe943d6301d17bad14bfe3641303384

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -23,8 +23,8 @@ instr_func opcode_entry[256] = {
     /* 0x4c - 0x4f*/ inv, inv, inv, inv,
     /* 0x50 - 0x53*/ push_r_v, push_r_v, push_r_v, push_r_v,//53 :push_r_v,51:push_r_v,52:push_r_v,50:push_r_v
     /* 0x54 - 0x57*/ push_r_v, push_r_v, push_r_v, push_r_v,//55push_r_v,57:push_r_v,56:push_r_v
-    /* 0x58 - 0x5b*/ inv, inv, inv, pop_r_v,//5b:pop_r_v
-    /* 0x5c - 0x5f*/ inv, pop_r_v, pop_r_v, pop_r_v,//5d:pop_r_v,5e:pop_r_v,5f:pop_r_v
+    /* 0x58 - 0x5b*/ inv, inv, inv, __ref_pop_r_v,//5b:pop_r_v
+    /* 0x5c - 0x5f*/ inv, __ref_pop_r_v, __ref_pop_r_v, __ref_pop_r_v,//5d:pop_r_v,5e:pop_r_v,5f:pop_r_v
     /* 0x60 - 0x63*/ inv, inv, inv, inv,
     /* 0x64 - 0x67*/ inv, inv, data_size_16, inv,
     /* 0x68 - 0x6b*/ inv, inv, push_i_b, inv,
