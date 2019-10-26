48063b34bf6d8290c669020aeeb9db26f972b3c7
monitor <tracer@njuics.org>
2019-10-26 17:34:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -38,3 +38,22 @@ make_instr_func(jmp_short)
 
         return 1 + new_data_size / 8;
 }
+
+make_instr_func(jmp_near_ind)
+{
+        OPERAND rel;
+        rel.type = OPR_IMM;
+        rel.sreg = SREG_CS;
+        rel.data_size = data_size;
+        rel.addr = eip + 1;
+
+        operand_read(&rel);
+
+        int offset = sign_ext(rel.val, data_size);
+        // thank Ting Xu from CS'17 for finding this bug
+        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
+
+        cpu.eip += offset;
+
+        return 1 + data_size / 8;
+}
