b55a0ec62cf46b7fcf68d727a76aae10ddf41eef
monitor <tracer@njuics.org>
2019-10-26 21:25:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -57,19 +57,19 @@ make_instr_func(jmp_near_indirect)
         OPERAND rel;
         rel.type = OPR_IMM;
         rel.sreg = SREG_CS;
-        rel.data_size = data_size;
+        //rel.data_size = data_size;
 
         rel.addr = eip + 1;
 
         operand_read(&rel);
         // thank Ting Xu from CS'17 for finding this bug
         //print_asm_1("jmp", "", 1 + data_size / 8, &rel);
-        if(data_size==16)
+        if(rel.data_size==16)
         { 
                 rel.val = sign_ext(rel.val, data_size);
                 cpu.eip=rel.val&0xffff;
         }
-        else if(data_size==32)
+        else
         {
                   
                 cpu.eip=rel.val;
