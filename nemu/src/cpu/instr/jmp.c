6e7e8f1a9dcf9e1532eb2f28b8cd0cdd21556377
tracer-pa-public <tracer@njuics.org>
2019-10-26 21:48:35 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  21:48:35 up 2 days, 18 min,  1 user,  load average: 0.45, 0.35, 0.18 359d377ede8710d074a71765449f10af8a53899

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -41,22 +41,9 @@ make_instr_func(jmp_short)
 
 make_instr_func(jmp_near_indirect)
 {
-/*
         OPERAND rel;
         rel.type = OPR_IMM;
         rel.sreg = SREG_CS;
-        rel.data_size = data_size;
-        rel.addr = eip + 1;
-        operand_read(&rel);
-        int offset = sign_ext(rel.val, data_size);
-        // thank Ting Xu from CS'17 for finding this bug
-        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
-        cpu.eip += offset;
-        return 1 + data_size / 8; 
-*/
-        OPERAND rel;
-        //rel.type = OPR_IMM;
-        //rel.sreg = SREG_CS;
         rel.data_size = data_size;
         //rel.addr = eip + 1;
 
