bd67062ccbd1e63128eff379253519ad388ffd5d
tracer-pa-public <tracer@njuics.org>
2019-10-27 01:30:51 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  01:30:51 up 2 days,  2:36,  1 user,  load average: 1.09, 0.73, 0.51 53005c48dc850f68ef5f53de54ae70871b86fb1b

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -61,7 +61,7 @@ instr_func opcode_entry[256] = {
     /* 0xe4 - 0xe7*/ inv, inv, inv, inv,
     /* 0xe8 - 0xeb*/ __ref_call_near, __ref_jmp_near, __ref_jmp_far_imm, __ref_jmp_short,//e8call_near, e9jmp_near,eb:jmp_short
     /* 0xec - 0xef*/ inv, inv, inv, inv,
-    /* 0xf0 - 0xf3*/ inv, break_point, inv, rep_repe,
+    /* 0xf0 - 0xf3*/ inv, break_point, inv, __ref_rep_repe,
     /* 0xf4 - 0xf7*/ hlt, inv, group_3_b, group_3_v,//group_3_v,10.17f7group_3_v->cmps_v,f6group_3_b->cmps_v,f5cmps_v
     /* 0xf8 - 0xfb*/ clc, inv, inv, inv,//10.17fbcmps_v,facmps_V,f9cmps_v
     /* 0xfc - 0xff*/ cld, inv, inv, group_5_indirect,//10.17ffcmps_v,fecmps_v,fdcmps_v
