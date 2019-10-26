39ee75fad0ed026a3538850e1f2fd30c0faa933b
monitor <tracer@njuics.org>
2019-10-26 15:59:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -45,6 +45,7 @@ extern uint8_t data_size;
 #include "cpu/instr/idiv.h"
 #include "cpu/instr/mul.h"
 #include "cpu/instr/stos.h"
+#include "cpu/instr/cmps.h"
 
 /* TODO: add more instructions here */
 
---
+++ nemu/include/cpu/instr/cmps.h
@@ -0,0 +1,8 @@
+#ifndef __INSTR_MUL_H__
+#define __INSTR_MUL_H__
+
+make_instr_func(cmps_v);
+make_instr_func(cmps_b);
+
+
+#endif
