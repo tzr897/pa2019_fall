ef325dab941af7c825bace700934b0fc07071488
tracer-pa-public <tracer@njuics.org>
2019-10-26 15:08:54 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  15:08:54 up 1 day, 19:11,  1 user,  load average: 0.08, 0.19, 0.23 521adf6c8816a987c41b6ff9b5784b4bb08f402c

--- nemu/include/cpu/instr/imul.h
+++ nemu/include/cpu/instr/imul.h
@@ -6,6 +6,6 @@ make_instr_func(imul_rm2a_b);
 make_instr_func(imul_rm2a_v);
 make_instr_func(imul_irm2r_v);
 make_instr_func(imul_i8rm2r_v);
-make_instr_func(imul_rm_v);
+//make_instr_func(imul_rm_v);
 
 #endif
