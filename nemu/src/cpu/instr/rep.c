91f00b61f532ddc7afe6a7415a8026ff577d0017
monitor <tracer@njuics.org>
2019-10-26 15:50:01 +0800

    git monitor commit.

---
+++ nemu/src/cpu/instr/rep.c
@@ -0,0 +1,15 @@
+#include "cpu/instr.h"
+
+make_instr_func(ret_near)
+{
+    OPERAND i;
+    i.data_size=data_size;
+    i.type=OPR_MEM;
+    i.addr=cpu.esp;
+    i.sreg=SREG_DS;
+    operand_read(&i);
+    cpu.esp+=data_size/8;
+    cpu.eip=i.val;
+
+    return 0;
+}
