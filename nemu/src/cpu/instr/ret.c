6cfda83ba596df188e1f6a425e149dca76cd37fb
tracer-pa-public <tracer@njuics.org>
2019-10-29 01:52:48 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  01:52:48 up  1:05,  1 user,  load average: 0.43, 0.32, 0.28 a62f9e75153b6099a681aaa61d105b9ed9973f65

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -17,35 +17,24 @@ make_instr_func(ret_near)
 
 make_instr_func(ret_near_imm16)
 {
-    //int offset;
     OPERAND i1,i2;
-
-    i1.data_size=data_size;
+    int new_data_size=32;
+    i1.data_size=new_data_size;
     i1.type=OPR_MEM;
     i1.addr=cpu.esp;
     i1.sreg=SREG_DS;
     operand_read(&i1);
-    cpu.esp+=data_size/8;
+    cpu.esp+=new_data_size/8;
     //offset=sign_ext(i1.val,data_size);
     cpu.eip=i1.val;
     
 
-    i2.data_size=data_size;
+    i2.data_size=new_data_size;
     i2.type=OPR_IMM;
     i2.addr=cpu.eip+1;
     i2.sreg=SREG_CS;
     operand_read(&i2);
     cpu.eip+=i2.val;
-
-
-    // int offset;
-    // OPERAND rel,i;
-    // rel.data_size=data_size;
-    // rel.type=OPR_IMM;
-    // rel.addr=cpu.eip+1;
-    // rel.sreg=SREG_CS;
-    // operand_read(&rel);
-
 
     return 0;
 }
