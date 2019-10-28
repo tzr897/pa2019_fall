a1db5565418d2a7da448ec95341febc19dde478b
tracer-pa-public <tracer@njuics.org>
2019-10-28 20:09:26 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:09:26 up 2 days,  8:00,  1 user,  load average: 0.43, 0.38, 0.25 bd8bd05fa2e55fc311cf97d84824417258f8f73c

--- nemu/include/cpu/instr_helper.h
+++ nemu/include/cpu/instr_helper.h
@@ -215,7 +215,7 @@ static inline bool inv_cc();
 	((cpu.eflags.SF^cpu.eflags.OF)==1)
 
 #define condition_le \
-	((1==cpu.eflags.ZF)&&(1==(cpu.eflags.SF^cpu.eflags.OF)))
+	((1==cpu.eflags.ZF)||(1==(cpu.eflags.SF^cpu.eflags.OF)))
 
 #define condition_ecxz \
 	cpu.ecx == 0
