2e72dc10c9eda0f3684197c94d79e4102356049a
tracer-pa-public <tracer@njuics.org>
2019-10-29 23:33:51 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:33:51 up 15 min,  1 user,  load average: 0.58, 0.53, 0.38 6bef2443d04580ad8e166a1def9c827910669303

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -42,30 +42,4 @@ make_instr_func(push_i_b)
 make_instr_impl_1op(push, rm, v)
 make_instr_impl_1op(push, r, v)
 make_instr_impl_1op(push, i, v)
-/* 
-#include "cpu/instr.h"
-
-make_instr_func(call_near)
-{
-    int offset;
-    OPERAND rel,i;
-    rel.data_size=data_size;
-    rel.type=OPR_IMM;
-    rel.addr=cpu.eip+1;
-    rel.sreg=SREG_CS;
-    operand_read(&rel);
-
-    i.data_size=data_size;
-    i.type=OPR_MEM;
-    cpu.esp-=data_size/8;
-    i.addr=cpu.esp;
-    i.sreg=SREG_DS;
-    i.val=cpu.eip+1+data_size/8;
-    operand_write(&i);
-    
-    offset=sign_ext(rel.val,data_size);
-    cpu.eip+=offset+1+data_size/8;
-    return 0;
-
-}
-*/
+//
