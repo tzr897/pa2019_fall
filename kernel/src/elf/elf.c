8dea6627e3f9381b75ac3f934e86b8f35fc98dad
monitor <tracer@njuics.org>
2019-11-01 14:43:01 +0800

    git monitor commit.

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -40,14 +40,17 @@ uint32_t loader()//renwu:shixian loader()
 			// remove this panic!!!
 			//panic("Please implement the loader");
 			//tihuan wei copy de guocheng
-			
+
 			//src:ph->p_offset
 			//dest:ph->p_vaddr
 			//size:ph->p_filesz
 			//memcpy(,,)
-/* TODO: copy the segment from the ELF file to its proper memory area */
 
+/* TODO: copy the segment from the ELF file to its proper memory area */
+			memcpy(ph->p_vaddr,ph->p_offset,ph->p_filesz);
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
+			memset(ph->p_vaddr+ph->p_filesz,0x0,ph->p_memsz-ph->filesz);
+
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
