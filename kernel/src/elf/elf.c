91d0691a05236d61633141a7981182c8a2d60b6b
tracer-pa-public <tracer@njuics.org>
2019-11-01 14:51:57 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:51:57 up  2:24,  1 user,  load average: 0.93, 0.63, 0.38 87432a3ba434574a1c0279ba9e640cdaffc7a33

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -45,11 +45,11 @@ uint32_t loader()//renwu:shixian loader()
 			//dest:ph->p_vaddr
 			//size:ph->p_filesz
 			//memcpy(,,)
-
+			memset()
 /* TODO: copy the segment from the ELF file to its proper memory area */
-			memcpy(ph->p_vaddr,ph->p_offset,ph->p_filesz);
+			memcpy((void *)ph->p_vaddr,(void *)ph->p_offset,ph->p_filesz);
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
-			memset(ph->p_vaddr+ph->p_filesz,0x0,ph->p_memsz-ph->filesz);
+			memset((void *)(ph->p_vaddr+ph->p_filesz),0x0,ph->p_memsz-ph->filesz);
 
 
 #ifdef IA32_PAGE
