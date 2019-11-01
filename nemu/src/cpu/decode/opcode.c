fe1531ffd3f5e67227e7e5f2e22542d036ea8885
monitor <tracer@njuics.org>
2019-11-01 16:38:01 +0800

    git monitor commit.

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -21,8 +21,8 @@ instr_func opcode_entry[256] = {
     /* 0x44 - 0x47*/ inv, inv, inc_r_v, inc_r_v,//47:inc_r_v
     /* 0x48 - 0x4b*/ dec_r_v, dec_r_v, inv, inv,
     /* 0x4c - 0x4f*/ dec_r_v, inv, inv, inv,
-    /* 0x50 - 0x53*/ push_r_v, push_r_v, push_r_v, push_r_v,//53 :push_r_v,51:push_r_v,52:push_r_v,50:push_r_v
-    /* 0x54 - 0x57*/ push_r_v, __ref_push_r_v, push_r_v, __ref_push_r_v,//55push_r_v,57:push_r_v,56:push_r_v
+    /* 0x50 - 0x53*/ push_r_v, __ref_push_r_v, push_r_v, __ref_push_r_v,//53 :push_r_v,51:push_r_v,52:push_r_v,50:push_r_v
+    /* 0x54 - 0x57*/ push_r_v, __ref_push_r_v, __ref_push_r_v, __ref_push_r_v,//55push_r_v,57:push_r_v,56:push_r_v
     /* 0x58 - 0x5b*/ pop_r_v, pop_r_v, pop_r_v, pop_r_v,//5b:pop_r_v
     /* 0x5c - 0x5f*/ pop_r_v, pop_r_v, pop_r_v, pop_r_v,//5d:pop_r_v,5e:pop_r_v,5f:pop_r_v
     /* 0x60 - 0x63*/ inv, inv, inv, inv,
@@ -35,7 +35,7 @@ instr_func opcode_entry[256] = {
     /* 0x7c - 0x7f*/ jl_short_, jge_short_, jle_short_, jg_short_,//7f:jg_short_,7e:jle_short_,7c:jl_short_,7d:jge_short_
     /* 0x80 - 0x83*/ group_1_b, group_1_v, nemu_trap, group_1_bv,//10.1783group_1_bv
     /* 0x84 - 0x87*/ test_r2rm_b, test_r2rm_v, inv, inv,//85:test_r2rm_v,84:test_r2rm_b
-    /* 0x88 - 0x8b*/ mov_r2rm_b, __ref_mov_r2rm_v, mov_rm2r_b, mov_rm2r_v,
+    /* 0x88 - 0x8b*/ mov_r2rm_b, __ref_mov_r2rm_v, mov_rm2r_b, __ref_mov_rm2r_v,
     /* 0x8c - 0x8f*/ inv, lea, inv, inv,//8d:lea
     /* 0x90 - 0x93*/ nop, inv, inv, inv,
     /* 0x94 - 0x97*/ inv, inv, inv, inv,
@@ -73,7 +73,7 @@ instr_func group_1_b_entry[8] =
 
 /* 0x81 */
 instr_func group_1_v_entry[8] =
-    {add_i2rm_v, inv, inv, sbb_i2rm_v, and_i2rm_v, sub_i2rm_v, inv, cmp_i2rm_v};//000:add_i2rm_v,101:sub_i2rm_v,111:cmp_i2rm_v,100:and_i2rm_v
+    {__ref_add_i2rm_v, inv, inv, sbb_i2rm_v, and_i2rm_v, sub_i2rm_v, inv, cmp_i2rm_v};//000:add_i2rm_v,101:sub_i2rm_v,111:cmp_i2rm_v,100:and_i2rm_v
 
 /* 0x83 */
 instr_func group_1_bv_entry[8] =
