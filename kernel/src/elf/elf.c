472313cddfd45ce741ef316fb52adf6cf16a9d47
monitor <tracer@njuics.org>
2019-11-01 16:20:01 +0800

    git monitor commit.

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -47,9 +47,9 @@ uint32_t loader()//renwu:shixian loader()
 			//memcpy(,,)
 			//memset()
 /* TODO: copy the segment from the ELF file to its proper memory area */
-			memcpy((void *)ph->p_vaddr,(void *)ph->p_offset,ph->p_filesz*8);
+			memcpy((void *)ph->p_vaddr,(void *)ph->p_offset,ph->p_filesz);
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
-			memset((void *)(ph->p_vaddr+ph->p_filesz*8),0x0,(ph->p_memsz-ph->p_filesz)*8);
+			memset((void *)(ph->p_vaddr+ph->p_filesz),0x0,(ph->p_memsz-ph->p_filesz));
 
 
 #ifdef IA32_PAGE
