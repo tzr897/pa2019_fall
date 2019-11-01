87b6616471dbf3487634f3d9ed0c049a95620b67
tracer-pa-public <tracer@njuics.org>
2019-11-01 16:17:43 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  16:17:43 up  3:16,  1 user,  load average: 0.35, 0.72, 0.57 a6d22b608b2fce4c1e8b32e66a3959b4db35f29d

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -26,46 +26,19 @@ make_instr_func(call_near)
 
 make_instr_func(call_near_indirect)
 {
-    // int offset;
-    // OPERAND rel,i;
-    // rel.data_size=data_size;
-    // rel.type=OPR_IMM;
-    // rel.addr=cpu.eip+1;
-    // rel.sreg=SREG_CS;
-    // operand_read(&rel);
-
-    // i.data_size=data_size;
-    // i.type=OPR_MEM;
-    // cpu.esp-=data_size/8;
-    // i.addr=cpu.esp;
-    // i.sreg=SREG_DS;
-    // i.val=cpu.eip+1+data_size/8;
-    // operand_write(&i);
-    
-    // offset=sign_ext(rel.val,data_size);
-    // cpu.eip+=offset+1+data_size/8;
-    // return 0;
-
-
-
-        OPERAND rel;
-
-        modrm_rm(cpu.eip+1, &rel);
+    OPERAND rel;
+    modrm_rm(cpu.eip+1, &rel);
         
-        rel.data_size = data_size;
+    rel.data_size = data_size;
+    operand_read(&rel);
 
-        
-        operand_read(&rel);
-
-        if(data_size==16)
-        {          
-                cpu.eip=rel.val&0xffff;
-        }
-        else
-        {
-                cpu.eip=rel.val;
-        }
-
-        return 0;
- 
+    if(data_size==16)
+    {          
+        cpu.eip=rel.val&0xffff;
+    }
+    else
+    {
+        cpu.eip=rel.val;
+    }
+    return 0; 
 }
