d35d5c5f5a9259ca76d489b3c46baec41689bfc8
monitor <tracer@njuics.org>
2019-11-21 17:21:01 +0800

    git monitor commit.

--- nemu/include/memory/cache.c
+++ nemu/include/memory/cache.c
@@ -2,23 +2,3 @@
 
 struct CacheLine vcache;
 
-uint32_t paddr_read(paddr_t paddr, size_t len)
-{
-    uint32_t ret=0;
-#ifdef CACHE_ENABLED
-        ret=cache_read(paddr,len,&L1_dcache);
-#else
-        ret=hw_mem_read(paddr,len);
-#endif
-    return ret;
-}
-
-void paddr_write(paddr_t paddr,size_t len,uint32_t data)
-{
-#ifdef CACHE_ENABLED
-        cache_write(paddr,len,data,&L1_dcache);
-#else
-        hw_mem_write(paddr,len,data);
-#endif
-
-}
