d6331053033fb31066a59fd22a2e730e347afcec
tracer-pa-public <tracer@njuics.org>
2019-11-21 17:23:12 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:23:12 up 1 day,  9:22,  1 user,  load average: 0.67, 0.50, 0.28 909e657bc4d4a492e9291cfb3462dabc137ca35

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -1,171 +1,162 @@
-// #include "nemu.h"
-// #include "cpu/reg.h"
-// #include "memory/memory.h"
+#include "nemu.h"
+#include "cpu/reg.h"
+#include "memory/memory.h"
 
-// #include <stdlib.h>
+#include <stdlib.h>
 
-// /* We use the POSIX regex functions to process regular expressions.
-//  * Type 'man regex' for more information about POSIX regex functions.
-//  */
-// #include <sys/types.h>
-// #include <regex.h>
+/* We use the POSIX regex functions to process regular expressions.
+ * Type 'man regex' for more information about POSIX regex functions.
+ */
+#include <sys/types.h>
+#include <regex.h>
 
-// enum
-// {
-// 	NOTYPE = 256,
-// 	EQ,
-// 	NUM,
-// 	REG,
-// 	SYMB,
-// 	//
-// 	HEX,
-// 	NE,
+enum
+{
+	NOTYPE = 256,
+	EQ,
+	NUM,
+	REG,
+	SYMB,
+	//
+	HEX,
+	NE,
 
-// 	/* TODO: Add more token types */
+	/* TODO: Add more token types */
 
-// };
+};
 
-// static struct rule
-// {
-// 	char *regex;
-// 	int token_type;
-// } rules[] = {
+static struct rule
+{
+	char *regex;
+	int token_type;
+} rules[] = {
 
-// 	/* TODO: Add more rules.
-// 	 * Pay attention to the precedence level of different rules.
-// 	 */
+	/* TODO: Add more rules.
+	 * Pay attention to the precedence level of different rules.
+	 */
 
-// 	{" +", NOTYPE}, // white space
-// 	{"\\+", '+'},
-// 	//
-// 	{"\\-", '-'},
-// 	{"\\*", '*'},
-// 	{"\\/", '/'},
-// 	//
-// 	{"[0-9]{1,10}", NUM}, //dec
-// 	{"-", '-'},
-// 	{"+", '+'},
-// 	{"*", '*'},
-// 	{"/", '/'},
-// 	//
-// 	{"\\(", '('},
-// 	{"\\)", ')'}
-// 	//
-// 	{"\\$e[abcd]x", REG},
-// 	{"\\$e[bs]p", REG},
-// 	{"\\$e[ds]i", REG},
-// 	//
-// 	{"[a-zA-Z]+[a-zA-Z0-9_]*",SYMB},
+	{" +", NOTYPE}, // white space
+	{"\\+", '+'},
+	//
+	{"\\-", '-'},
+	{"\\*", '*'},
+	{"\\/", '/'},
+	//
+	{"[0-9]{1,10}", NUM}, //dec
+	{"-", '-'},
+	{"+", '+'},
+	{"*", '*'},
+	{"/", '/'},
+	//
+	{"\\(", '('},
+	{"\\)", ')'}
+	//
+	{"\\$e[abcd]x", REG},
+	{"\\$e[bs]p", REG},
+	{"\\$e[ds]i", REG},
+	//
+	{"[a-zA-Z]+[a-zA-Z0-9_]*",SYMB},
 
 
-// 	//
-// 	{"==", EQ},
-// 	{"0[xX][0-9a-fA-F]+", HEX},
-// 	//
-// 	{"!", '!'},
-// 	{"!=", NE},
-// 	{"%", '%'},
+	//
+	{"==", EQ},
+	{"0[xX][0-9a-fA-F]+", HEX},
+	//
+	{"!", '!'},
+	{"!=", NE},
+	{"%", '%'},
 
 	
 
 
 
-// };
+};
 
-// #define NR_REGEX (sizeof(rules) / sizeof(rules[0]))
+#define NR_REGEX (sizeof(rules) / sizeof(rules[0]))
 
-// static regex_t re[NR_REGEX];
+static regex_t re[NR_REGEX];
 
-// /* Rules are used for more times.
-//  * Therefore we compile them only once before any usage.
-//  */
-// void init_regex()
-// {
-// 	int i;
-// 	char error_msg[128];
-// 	int ret;
+/* Rules are used for more times.
+ * Therefore we compile them only once before any usage.
+ */
+void init_regex()
+{
+	int i;
+	char error_msg[128];
+	int ret;
 
-// 	for (i = 0; i < NR_REGEX; i++)
-// 	{
-// 		ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-// 		if (ret != 0)
-// 		{
-// 			regerror(ret, &re[i], error_msg, 128);
-// 			assert(ret != 0);
-// 		}
-// 	}
-// }
+	for (i = 0; i < NR_REGEX; i++)
+	{
+		ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+		if (ret != 0)
+		{
+			regerror(ret, &re[i], error_msg, 128);
+			assert(ret != 0);
+		}
+	}
+}
 
-// typedef struct token
-// {
-// 	int type;
-// 	char str[32];
-// } Token;
+typedef struct token
+{
+	int type;
+	char str[32];
+} Token;
 
-// Token tokens[32];
-// int nr_token;
+Token tokens[32];
+int nr_token;
 
-// static bool make_token(char *e)
-// {
-// 	int position = 0;
-// 	int i;
-// 	regmatch_t pmatch;
+static bool make_token(char *e)
+{
+	int position = 0;
+	int i;
+	regmatch_t pmatch;
 
-// 	nr_token = 0;
+	nr_token = 0;
 
-// 	while (e[position] != '\0')
-// 	{
-// 		/* Try all rules one by one. */
-// 		for (i = 0; i < NR_REGEX; i++)
-// 		{
-// 			if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0)
-// 			{
-// 				char *substr_start = e + position;
-// 				int substr_len = pmatch.rm_eo;
+	while (e[position] != '\0')
+	{
+		/* Try all rules one by one. */
+		for (i = 0; i < NR_REGEX; i++)
+		{
+			if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0)
+			{
+				char *substr_start = e + position;
+				int substr_len = pmatch.rm_eo;
 
-// 				printf("match regex[%d] at position %d with len %d: %.*s", i, position, substr_len, substr_len, substr_start);
-// 				position += substr_len;
+				printf("match regex[%d] at position %d with len %d: %.*s", i, position, substr_len, substr_len, substr_start);
+				position += substr_len;
 
-// 				/* TODO: Now a new token is recognized with rules[i]. 
-// 				 * Add codes to perform some actions with this token.
-// 				 */
+				/* TODO: Now a new token is recognized with rules[i]. 
+				 * Add codes to perform some actions with this token.
+				 */
 
-// 				switch (rules[i].token_type)
-// 				{
-// 					/*
-// 					NOTYPE = 256,
-// 	EQ,	
-// 	NUM,
-// 	REG,
-// 	SYMB,
-// 	//
-// 	HEX,
-// 	NE,
-// 					 */
-// 					case NOTYPE:
-// 					case NUM:
-// 					case REG:
-// 					case SYMB:
-// 					case HEX:
+				switch (rules[i].token_type)
+				{
+					
+					// case NOTYPE:
+					// case NUM:
+					// case REG:
+					// case SYMB:
+					// case HEX:
 						
-// 					default:
-// 						tokens[nr_token].type = rules[i].token_type;
-// 						nr_token++;
-// 				}
+					default:
+						tokens[nr_token].type = rules[i].token_type;
+						nr_token++;
+				}
 
-// 				break;
-// 			}
-// 		}
+				break;
+			}
+		}
 
-// 		if (i == NR_REGEX)
-// 		{
-// 			printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-// 			return false;
-// 		}
-// 	}
+		if (i == NR_REGEX)
+		{
+			printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+			return false;
+		}
+	}
 
-// 	return true;
-// }
+	return true;
+}
 
 
 
@@ -209,8 +200,8 @@
 
 
 
-// //s:当前待求值表达式在tokens[]数组中的起始位置
-// //e:当前待求值表达式在tokens[]数组中的结束位置
+//s:当前待求值表达式在tokens[]数组中的起始位置
+//e:当前待求值表达式在tokens[]数组中的结束位置
 // uint32_t eval(int s, int e, bool *success)
 // {
 // 	// if(s>e)
@@ -252,23 +243,21 @@
 	
 // }
 
-// uint32_t expr(char *e, bool *success)/////////
-// {
-// 	// if (!make_token(e))
-// 	// {
-// 	// 	*success = false;
-// 	// 	return 0;
-// 	// }
+uint32_t expr(char *e, bool *success)/////////
+{
+	if (!make_token(e))
+	{
+		*success = false;
+		return 0;
+	}
 
-// 	// for(uint32_t i=0;i<nr_token;i++)
-// 	// {
-// 	// 	if(tokens[i].type=='*' && (i==0||tokens[i-1].type==))//bu
-// 	// 	{
-// 	// 		tokens[i].type=DEREF;
-// 	// 	}
-// 	// }
-
-
+	// for(uint32_t i=0;i<nr_token;i++)
+	// {
+	// 	if(tokens[i].type=='*' && (i==0||tokens[i-1].type==))//bu
+	// 	{
+	// 		tokens[i].type=DEREF;
+	// 	}
+	// }
 
 
 
@@ -296,8 +285,10 @@
 
 
 
-// 	// printf("\nPlease implement expr at expr.c\n");
-// 	// assert(0);
 
-// 	// return 0;
-// }
+
+	printf("\nPlease implement expr at expr.c\n");
+	assert(0);
+
+	return 0;
+}
