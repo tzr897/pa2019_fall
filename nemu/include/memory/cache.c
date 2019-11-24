fdf5b51b312804da433a8a2f5094b2c67969065d
monitor <tracer@njuics.org>
2019-11-24 22:47:01 +0800

    git monitor commit.

--- nemu/include/memory/cache.c
+++ nemu/include/memory/cache.c
@@ -1,4 +1,11 @@
+#include <stdio.h>
+#include <memory.h>
+#include "nemu.h"
+#include "cpu/cpu.h"
+#include "memory/memory.h"
+#include "device/mm_io.h"
 #include "memory/cache.h"
+#include <stdlib.h>
 
 struct CacheLine vcache;
 
