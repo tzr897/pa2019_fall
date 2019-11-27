a6992b8000b1a0e7ade937a181aaf366882cf330
tracer-pa-public <tracer@njuics.org>
2019-11-28 01:15:20 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  01:15:20 up 1 day, 17:45,  1 user,  load average: 0.15, 0.12, 0.11 30ad43894dc8f608ed0802b5efccce26f3582f79

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -99,7 +99,7 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine *cache)
             {
                 size_t out=baddr+len-64;
                 uint32_t data1=0;
-                uint32_t data2=0;
+                //uint32_t data2=0;
                 data1=(data>>((64-baddr)*8));
                 cache_write(paddr+(64-baddr), out, data1, cache);
                 cache_write(paddr, 64-baddr, data, cache); 
