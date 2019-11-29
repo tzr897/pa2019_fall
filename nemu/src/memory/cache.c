21a1b727ed2dd393b5b69fefc4c74ba3a03d87b4
tracer-pa-public <tracer@njuics.org>
2019-11-29 14:07:16 +0800

    >  clean 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:07:16 up 1 day, 23:00,  1 user,  load average: 0.90, 0.64, 0.42 4082c9cabbc2550a6660d5af882259e1c2af3eef

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -53,7 +53,7 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine *cache)
     //i yiqueding
     if((baddr+len)<=64)//bukuahang
     {
-        memcpy(&ret, &cache[group*8+i].block+baddr, len);
+        memcpy(&ret, &Cache[group*8+i].block+baddr, len);
     }
     else//kuahang
     {
@@ -87,7 +87,7 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine *cache)
         {
             if((baddr+len)<=64)
             {
-                memcpy(&cache[group*8+i].block+baddr, &data, len);
+                memcpy(&Cache[group*8+i].block+baddr, &data, len);
             }
             else
             {
