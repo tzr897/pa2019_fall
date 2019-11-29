1fdd816fd6926cc3bb195219e3b8e0b8494f3e04
monitor <tracer@njuics.org>
2019-11-29 12:47:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -44,8 +44,7 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine *cache)
             {
                 srand((unsigned)time(NULL));
                 i=rand()%8;
-            }
-            
+            } 
             cache[group*8+i].valid_bit=1;
             cache[group*8+i].tag=mark;
             memcpy(cache[group*8+i].block, hw_mem+paddr-baddr, 64);
@@ -98,12 +97,6 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine *cache)
                 cache_write(paddr+(64-baddr), out, data1, Cache);
                 cache_write(paddr, 64-baddr, data, Cache); 
             }
-            
-            
         }
-        
     }
-
-
-
 }
