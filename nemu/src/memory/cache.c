b7721038579cb95fb7ae4fd6640ca94fd1f58706
monitor <tracer@njuics.org>
2019-11-29 14:31:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -52,7 +52,7 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine *cache)
         }
     //}
     //i yiqueding
-    if((baddr+len)<64)//bukuahang
+    if((baddr+len)<=64)//bukuahang
     {
         memcpy(&ret, &cache[group*8+i].block+baddr, len);
     }
@@ -87,7 +87,7 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine *cache)
         }
         else
         {
-            if((baddr+len)<64)
+            if((baddr+len)<=64)
             {
                 memcpy(&cache[group*8+i].block+baddr, &data, len);
             }
