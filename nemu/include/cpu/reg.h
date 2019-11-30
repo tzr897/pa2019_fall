bdc92cc90810cc61317d701a25bc39f3ff929725
tracer-pa-public <tracer@njuics.org>
2019-12-01 01:30:28 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  01:30:28 up 2 days,  5:27,  1 user,  load average: 0.41, 0.20, 0.12 8bf15ab9e4af837f12dbb09b381e53fc14fe7e3f

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -3,6 +3,55 @@
 
 #include "nemu.h"
 
+//11.28 begin
+
+typedef struct//GDTR 
+{
+	uint32_t limit :16;
+	uint32_t base :32;
+}GDTR;
+
+typedef union//CR0 
+{
+	struct 
+	{
+		uint32_t pe :1;
+		uint32_t mp :1;
+		uint32_t em :1;
+		uint32_t ts :1;
+		uint32_t et :1;
+		uint32_t reserve :26;
+		uint32_t pg :1;
+	};
+	uint32_t val; 
+}CR0;
+
+typedef struct//SegReg 
+{
+// the 16-bit visible part, i.e., the selector
+	union 
+	{
+		uint16_t val;
+		struct 
+		{
+			uint32_t rpl :2;
+			uint32_t ti :1;
+			uint32_t index :13;
+		};
+	};
+	// the invisible part, i.e., cache part
+	struct 
+	{
+		uint32_t base;
+		uint32_t limit;
+		uint32_t type :5;
+		uint32_t privilege_level :2;
+		uint32_t soft_use :1;
+	};
+}SegReg;
+
+//11.28 end
+
 // define the structure of registers
 typedef struct
 {
@@ -99,54 +148,7 @@ typedef struct
 
 
 
-//11.28 begin
 
-typedef struct//GDTR 
-{
-	uint32_t limit :16;
-	uint32_t base :32;
-}GDTR;
-
-typedef union//CR0 
-{
-	struct 
-	{
-		uint32_t pe :1;
-		uint32_t mp :1;
-		uint32_t em :1;
-		uint32_t ts :1;
-		uint32_t et :1;
-		uint32_t reserve :26;
-		uint32_t pg :1;
-	};
-	uint32_t val; 
-}CR0;
-
-typedef struct//SegReg 
-{
-// the 16-bit visible part, i.e., the selector
-	union 
-	{
-		uint16_t val;
-		struct 
-		{
-			uint32_t rpl :2;
-			uint32_t ti :1;
-			uint32_t index :13;
-		};
-	};
-	// the invisible part, i.e., cache part
-	struct 
-	{
-		uint32_t base;
-		uint32_t limit;
-		uint32_t type :5;
-		uint32_t privilege_level :2;
-		uint32_t soft_use :1;
-	};
-}SegReg;
-
-//11.28 end
 
 
 
