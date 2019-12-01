5eacb1894a336ea7497cd633871cd4abda24b616
monitor <tracer@njuics.org>
2019-12-01 14:13:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -19,7 +19,58 @@ void load_sreg(uint8_t sreg)
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
 	SegDesc s;
-	//assert(s.)
 
-	
+	cpu.segReg[sreg].base=(s.base_31_24<<24)|(s.base_23_16<<16)|s.base_15_0;
+	cpu.segReg[sreg].limit=(s.limit_19_16<<16)|s.limit_15_0;
+	cpu.segReg[sreg].privilege_level=s.privilege_level;
+	assert(s.present==1);
+	assert(s.granularity=1);
 }
+/*
+typedef struct//SegReg 
+{
+// the 16-bit visible part, i.e., the selector
+	union 
+	{
+		uint16_t val;
+		struct 
+		{
+			uint32_t rpl :2;
+			uint32_t ti :1;
+			uint32_t index :13;
+		};
+	};
+	// the invisible part, i.e., cache part
+	struct 
+	{
+		uint32_t base;
+		uint32_t limit;
+		uint32_t type :5;
+		uint32_t privilege_level :2;
+		uint32_t soft_use :1;
+	};
+}SegReg;
+
+typedef union SegmentDescriptor {
+	struct
+	{
+		uint32_t limit_15_0 : 16;
+		uint32_t base_15_0 : 16;
+		uint32_t base_23_16 : 8;
+		uint32_t type : 4;
+		uint32_t segment_type : 1;
+		uint32_t privilege_level : 2;
+		uint32_t present : 1;
+		uint32_t limit_19_16 : 4;
+		uint32_t soft_use : 1;
+		uint32_t operation_size : 1;
+		uint32_t pad0 : 1;
+		uint32_t granularity : 1;
+		uint32_t base_31_24 : 8;
+	};
+	uint32_t val[2];
+} SegDesc;
+
+uint32_t segment_translate(uint32_t vaddr, uint8_t sreg);
+void load_sreg(uint8_t sreg);
+ */
