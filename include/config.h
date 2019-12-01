2b0d2b74a4d9a0db1bcab4949296a777d2602781
tracer-pa-public <tracer@njuics.org>
2019-12-01 16:02:55 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  16:02:55 up 2 days,  8:52,  1 user,  load average: 0.28, 0.27, 0.21 e172b2dd908a97bb4973e5fba0398be51b35efe3

--- include/config.h
+++ include/config.h
@@ -5,7 +5,7 @@
 
 // PA 3
 #define CACHE_ENABLED//11.21
-//#define IA32_SEG//11.30             // protect mode enable
+#define IA32_SEG//11.30             // protect mode enable
 //#define IA32_PAGE            // virtual memory management is now complete
 //#define TLB_ENABLED
 
