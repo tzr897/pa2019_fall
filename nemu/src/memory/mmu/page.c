6e056fd2f36b4686e0d06bffdddb60f840704035
monitor <tracer@njuics.org>
2019-12-08 14:49:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -58,11 +58,11 @@ paddr_t page_translate(laddr_t laddr)
 	paddr_t res = 0;
 
 	PDE pde;
-	pde = *((uint32_t)hw_mem + cpu.cr3.pdbr<<12 + dir*4);
+	pde.val = paddr_read(((uint32_t)hw_mem + cpu.cr3.pdbr<<12 + dir*4), 4);
 	assert(pde.present==1);
 
 	PTE pte;
-	pte = *((uint32_t)hw_mem + pde.page_frame<<12 + page*4);
+	pte.val = paddr_read(((uint32_t)hw_mem + pde.page_frame<<12 + page*4), 4);
 	assert(pte.present==1);
 
 	res = pte.page_frame<<12 | offset;
