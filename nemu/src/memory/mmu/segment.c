f13365c52c497113c776292331b29391b14c7556
tracer-pa-public <tracer@njuics.org>
2019-12-08 17:37:21 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:37:21 up 3 days,  3:00,  1 user,  load average: 0.16, 0.38, 0.28 e20f667cadf42d96ed3a7d19dda6fd41e72e9a5

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -20,14 +20,14 @@ void load_sreg(uint8_t sreg)
 	SegDesc s;
 	uint32_t taddr;
 	taddr = (uint32_t)hw_mem + (cpu.segReg[sreg].index*8) + cpu.gdtr.base;
-	s.val[0]=vaddr_read(taddr, 4);
-	s.val[1]=vaddr_read(taddr+4, 4);
+	s.val[0]=paddr_read(taddr, 4);
+	s.val[1]=paddr_read(taddr+4, 4);
 	cpu.segReg[sreg].base=(s.base_31_24<<24)|(s.base_23_16<<16)|s.base_15_0;
 	cpu.segReg[sreg].limit=(s.limit_19_16<<16)|s.limit_15_0;
 	cpu.segReg[sreg].privilege_level=s.privilege_level;
 	cpu.segReg[sreg].soft_use=s.soft_use;
 
 	assert(cpu.segReg[sreg].base==0);
-	assert(s.present==1);
+	//assert(s.present==1);
 	assert(s.granularity==1);
 }
