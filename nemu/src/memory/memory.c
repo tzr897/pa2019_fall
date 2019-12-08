a19c1d37c3f21146e4c8737a51d1348955fa7882
tracer-pa-public <tracer@njuics.org>
2019-12-09 01:27:42 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  01:27:42 up 3 days,  5:23,  1 user,  load average: 0.10, 0.20, 0.18 2e9481758db3b426102bbbd673ca57ff62348317

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -117,7 +117,7 @@ uint32_t vaddr_read(vaddr_t vaddr, uint8_t sreg, size_t len)
 	uint32_t laddr=vaddr;
 	if(cpu.cr0.pe)//11.30
 	{
-		laddr=segment_translate(vaddr,sreg);
+		//laddr=segment_translate(vaddr,sreg);
 	}
 	return laddr_read(laddr, len);
 #endif
