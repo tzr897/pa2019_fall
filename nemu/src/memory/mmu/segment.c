cda7eed60aca553f1529c7d4f9df3b6322341ebe
tracer-pa-public <tracer@njuics.org>
2019-12-08 17:55:49 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:55:49 up 3 days,  3:18,  1 user,  load average: 0.08, 0.07, 0.17 785e9133d116de929b922982b110bc2f6c559519

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -28,6 +28,6 @@ void load_sreg(uint8_t sreg)
 	cpu.segReg[sreg].soft_use=s.soft_use;
 
 	assert(cpu.segReg[sreg].base==0);
-	//assert(s.present==1);
-	//assert(s.granularity==1);
+	assert(s.present==1);
+	assert(s.granularity==1);
 }
