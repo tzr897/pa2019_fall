0abc6c97693d8f79e71de386b370f6e9ab035840
monitor <tracer@njuics.org>
2019-12-09 22:38:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -52,21 +52,21 @@ paddr_t page_translate(laddr_t laddr)
 // 	};
 // 	uint32_t val;
 // } PTE;
-	uint32_t dir = laddr >> 22;
-	uint32_t page = (laddr >> 12) & 0x3ff;
-	uint32_t offset = laddr & 0xfff;
-	paddr_t res = 0;
+	// uint32_t dir = laddr >> 22;
+	// uint32_t page = (laddr >> 12) & 0x3ff;
+	// uint32_t offset = laddr & 0xfff;
+	// paddr_t res = 0;
 
-	PDE pde;
-	pde.val = paddr_read(((uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + dir*4), 4);
-	assert(pde.present==1);
+	// PDE pde;
+	// pde.val = paddr_read(((uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + dir*4), 4);
+	// assert(pde.present==1);
 
-	PTE pte;
-	pte.val = paddr_read(((uint32_t)hw_mem + (pde.page_frame<<12) + page*4), 4);
-	assert(pte.present==1);
+	// PTE pte;
+	// pte.val = paddr_read(((uint32_t)hw_mem + (pde.page_frame<<12) + page*4), 4);
+	// assert(pte.present==1);
 
-	res = pte.page_frame<<12 | offset;
-	return res;
+	// res = pte.page_frame<<12 | offset;
+	// return res;
 
 
 
