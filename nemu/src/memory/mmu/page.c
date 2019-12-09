06521e49c575d35a9bd7840f2bd964df27de37ae
tracer-pa-public <tracer@njuics.org>
2019-12-10 00:39:52 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  00:39:52 up 3 days, 12:42,  1 user,  load average: 0.02, 0.14, 0.15 e789f56779e3c621fe32db8d82303a3ffa0b08c5

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -74,7 +74,7 @@ paddr_t page_translate(laddr_t laddr)
 
 	PDE *pde;
 	pde=(PDE*)((uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + dir*4);
-	assert(pde->present==1);
+	//assert(pde->present==1);
 
 	PTE *pte;
 	pte=(PTE*)((uint32_t)hw_mem + (pde->page_frame<<12) + page*4);
