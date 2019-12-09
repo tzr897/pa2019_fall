53025201d69ace7835c3934203b0a2f000dcff38
tracer-pa-public <tracer@njuics.org>
2019-12-10 00:28:44 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  00:28:44 up 3 days, 12:31,  1 user,  load average: 0.36, 0.34, 0.16 7f1184c94dd93cf017f93e829a0b6e92eda698a7

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -57,31 +57,31 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t offset = laddr & 0xfff;
 	paddr_t res = 0;
 
-	PDE pde;
-	pde.val = paddr_read(((uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + dir*4), 4);
-	assert(pde.present==1);
+	// PDE pde;
+	// pde.val = paddr_read(((uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + dir*4), 4);
+	// assert(pde.present==1);
 
-	PTE pte;
-	pte.val = paddr_read(((uint32_t)hw_mem + (pde.page_frame<<12) + page*4), 4);
-	assert(pte.present==1);
+	// PTE pte;
+	// pte.val = paddr_read(((uint32_t)hw_mem + (pde.page_frame<<12) + page*4), 4);
+	// assert(pte.present==1);
 
-	res = pte.page_frame<<12 | offset;
-	return res;
-
-
-
-
-
-	// PDE *pde;
-	// pde=(PDE*)((uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + dir*4);
-	// assert(pde->present==1);
-
-	// PTE *pte;
-	// pte=(PTE*)((uint32_t)hw_mem + (pde->page_frame<<12) + page*4);
-	// assert(pte->present==1);
-
-	// res=(pte->page_frame<<12) | offset;
+	// res = pte.page_frame<<12 | offset;
 	// return res;
+
+
+
+
+
+	PDE *pde;
+	pde=(PDE*)((uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + dir*4);
+	assert(pde->present==1);
+
+	PTE *pte;
+	pte=(PTE*)((uint32_t)hw_mem + (pde->page_frame<<12) + page*4);
+	assert(pte->present==1);
+
+	res=(pte->page_frame<<12) | offset;
+	return res;
 
 
 #else
