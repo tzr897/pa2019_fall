105ca99bd67a3af80c12370aaa4f95dff13035a2
monitor <tracer@njuics.org>
2019-12-10 09:19:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -8,50 +8,6 @@ paddr_t page_translate(laddr_t laddr)
 #ifndef TLB_ENABLED
 	// printf("\nPlease implement page_translate()\n");
 	// assert(0);
-// typedef union//CR3
-// {
-// 	struct 
-// 	{
-// 		uint32_t reserve:12;
-// 		uint32_t pdbr :20;
-// 	};
-// 	uint32_t val; 
-// }CR3;
-
-// /* the 32bit Page Directory(first level page table) data structure */
-// typedef union PageDirectoryEntry {
-// 	struct
-// 	{
-// 		uint32_t present : 1;
-// 		uint32_t read_write : 1;
-// 		uint32_t user_supervisor : 1;
-// 		uint32_t page_write_through : 1;
-// 		uint32_t page_cache_disable : 1;
-// 		uint32_t accessed : 1;
-// 		uint32_t pad0 : 6;
-// 		uint32_t page_frame : 20;
-// 	};
-// 	uint32_t val;
-// } PDE;
-
-// /* the 32bit Page Table Entry(second level page table) data structure */
-// typedef union PageTableEntry {
-// 	struct
-// 	{
-// 		uint32_t present : 1;
-// 		uint32_t read_write : 1;
-// 		uint32_t user_supervisor : 1;
-// 		uint32_t page_write_through : 1;
-// 		uint32_t page_cache_disable : 1;
-// 		uint32_t accessed : 1;
-// 		uint32_t dirty : 1;
-// 		uint32_t pad0 : 1;
-// 		uint32_t global : 1;
-// 		uint32_t pad1 : 3;
-// 		uint32_t page_frame : 20;
-// 	};
-// 	uint32_t val;
-// } PTE;
 	uint32_t dir = (laddr >> 22) & 0x3ff;
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
