59bf2a7a9b5f5687d21c96b5e0ae46bc9f70bb99
monitor <tracer@njuics.org>
2019-12-23 11:39:01 +0800

    git monitor commit.

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,6 +7,12 @@ void raise_intr(uint8_t intr_no)
 #ifdef IA32_INTR
 	// printf("Please implement raise_intr()");
 	// assert(0);
+// Trigger an exception/interrupt with 'intr_no'
+// 'intr_no' is the index to the IDT
+// Push EFLAGS, CS, and EIP
+// Find the IDT entry using 'intr_no'
+// Clear IF if it is an interrupt
+// Set EIP to the entry of the interrupt handl
 
 
 
@@ -20,7 +26,6 @@ void raise_intr(uint8_t intr_no)
 
 
 
-	
 #endif
 }
 
