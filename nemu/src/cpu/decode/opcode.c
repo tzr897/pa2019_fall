beea116f13b900448e9d1842baa397bb74ba654e
monitor <tracer@njuics.org>
2020-01-15 01:45:01 +0800

    git monitor commit.

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -31,7 +31,7 @@ instr_func opcode_entry[256] = {
     /* 0x6c - 0x6f*/ inv, inv, inv, inv,
     /* 0x70 - 0x73*/ inv, inv, jb_short_, jae_short_,
     /* 0x74 - 0x77*/ je_short_, jne_short_, jna_short_, ja_short_,//74je_short_,75:jne_short_,76:jna_short_
-    /* 0x78 - 0x7b*/ js_short_, inv, inv, inv,
+    /* 0x78 - 0x7b*/ js_short_, jns_short_, inv, inv,
     /* 0x7c - 0x7f*/ jl_short_, jge_short_, jle_short_, jg_short_,//7f:jg_short_,7e:jle_short_,7c:jl_short_,7d:jge_short_
     /* 0x80 - 0x83*/ group_1_b, group_1_v, nemu_trap, group_1_bv,//10.1783group_1_bv
     /* 0x84 - 0x87*/ test_r2rm_b, test_r2rm_v, inv, inv,//85:test_r2rm_v,84:test_r2rm_b
@@ -52,7 +52,7 @@ instr_func opcode_entry[256] = {
     /* 0xc0 - 0xc3*/ group_2_b, group_2_v, ret_near_imm16, ret_near,//c2:ret_near
     /* 0xc4 - 0xc7*/ inv, inv, mov_i2rm_b, mov_i2rm_v,
     /* 0xc8 - 0xcb*/ inv, leave, inv, inv,//c9:leave
-    /* 0xcc - 0xcf*/ inv, __ref_int_, inv, iret,
+    /* 0xcc - 0xcf*/ inv, int_, inv, iret,
     /* 0xd0 - 0xd3*/ group_2_1b, group_2_1v, group_2_cb, group_2_cv,
     /* 0xd4 - 0xd7*/ inv, inv, inv, inv,
     /* 0xd8 - 0xdb*/ group_x87_d8, group_x87_d9, group_x87_da, group_x87_db,
@@ -63,7 +63,7 @@ instr_func opcode_entry[256] = {
     /* 0xec - 0xef*/ inv, inv, inv, inv,
     /* 0xf0 - 0xf3*/ inv, break_point, inv, rep_repe,
     /* 0xf4 - 0xf7*/ hlt, inv, group_3_b, group_3_v,//group_3_v,10.17f7group_3_v->cmps_v,f6group_3_b->cmps_v,f5cmps_v
-    /* 0xf8 - 0xfb*/ clc, inv, __ref_cli, __ref_sti,//10.17fbcmps_v,facmps_V,f9cmps_v
+    /* 0xf8 - 0xfb*/ clc, inv, cli, sti,//10.17fbcmps_v,facmps_V,f9cmps_v
     /* 0xfc - 0xff*/ cld, inv, inv, group_5_indirect,//10.17ffcmps_v,fecmps_v,fdcmps_v
 };
 
