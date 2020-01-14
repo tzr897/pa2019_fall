a6e05f8b63a7c8cdd7c72ef03d5e310c647306d5
tracer-pa-public <tracer@njuics.org>
2020-01-15 01:31:59 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  01:31:59 up 4 days, 18:47,  1 user,  load average: 1.32, 0.59, 0.39 b2ae42f51a5b7e31a1ce2333f716ef2e5aa6c99

--- nemu/src/cpu/instr/popa.c
+++ nemu/src/cpu/instr/popa.c
@@ -2,65 +2,65 @@
 
 make_instr_func(popa)
 {
-    data_size=32;
-    //edi
-    opr_dest.type=OPR_MEM;
-    opr_dest.addr=cpu.esp;
-    opr_dest.data_size=data_size;
-    opr_dest.sreg=SREG_DS;
-    operand_read(&opr_dest);
-    cpu.edi=opr_dest.val;  
-    cpu.esp+=data_size/8;
-    //esi
-    opr_dest.type=OPR_MEM;
-    opr_dest.addr=cpu.esp;
-    opr_dest.data_size=data_size;
-    opr_dest.sreg=SREG_DS;
-    operand_read(&opr_dest);
-    cpu.esi=opr_dest.val;  
-    cpu.esp+=data_size/8;
-    //ebp
-    opr_dest.type=OPR_MEM;
-    opr_dest.addr=cpu.esp;
-    opr_dest.data_size=data_size;
-    opr_dest.sreg=SREG_DS;
-    operand_read(&opr_dest);
-    cpu.ebp=opr_dest.val;  
-    cpu.esp+=data_size/8;
-    //skip esp
-    cpu.esp+=data_size/8;
-    //ebx
-    opr_dest.type=OPR_MEM;
-    opr_dest.addr=cpu.esp;
-    opr_dest.data_size=data_size;
-    opr_dest.sreg=SREG_DS;
-    operand_read(&opr_dest);
-    cpu.ebx=opr_dest.val;  
-    cpu.esp+=data_size/8;
-    //edx
-    opr_dest.type=OPR_MEM;
-    opr_dest.addr=cpu.esp;
-    opr_dest.data_size=data_size;
-    opr_dest.sreg=SREG_DS;
-    operand_read(&opr_dest);
-    cpu.edx=opr_dest.val;  
-    cpu.esp+=data_size/8;
-    //ecx
-    opr_dest.type=OPR_MEM;
-    opr_dest.addr=cpu.esp;
-    opr_dest.data_size=data_size;
-    opr_dest.sreg=SREG_DS;
-    operand_read(&opr_dest);
-    cpu.ecx=opr_dest.val;  
-    cpu.esp+=data_size/8;
-    //eax
-    opr_dest.type=OPR_MEM;
-    opr_dest.addr=cpu.esp;
-    opr_dest.data_size=data_size;
-    opr_dest.sreg=SREG_DS;
-    operand_read(&opr_dest);
-    cpu.eax=opr_dest.val;  
-    cpu.esp+=data_size/8;
+    // data_size=32;
+    // //edi
+    // opr_dest.type=OPR_MEM;
+    // opr_dest.addr=cpu.esp;
+    // opr_dest.data_size=data_size;
+    // opr_dest.sreg=SREG_DS;
+    // operand_read(&opr_dest);
+    // cpu.edi=opr_dest.val;  
+    // cpu.esp+=data_size/8;
+    // //esi
+    // opr_dest.type=OPR_MEM;
+    // opr_dest.addr=cpu.esp;
+    // opr_dest.data_size=data_size;
+    // opr_dest.sreg=SREG_DS;
+    // operand_read(&opr_dest);
+    // cpu.esi=opr_dest.val;  
+    // cpu.esp+=data_size/8;
+    // //ebp
+    // opr_dest.type=OPR_MEM;
+    // opr_dest.addr=cpu.esp;
+    // opr_dest.data_size=data_size;
+    // opr_dest.sreg=SREG_DS;
+    // operand_read(&opr_dest);
+    // cpu.ebp=opr_dest.val;  
+    // cpu.esp+=data_size/8;
+    // //skip esp
+    // cpu.esp+=data_size/8;
+    // //ebx
+    // opr_dest.type=OPR_MEM;
+    // opr_dest.addr=cpu.esp;
+    // opr_dest.data_size=data_size;
+    // opr_dest.sreg=SREG_DS;
+    // operand_read(&opr_dest);
+    // cpu.ebx=opr_dest.val;  
+    // cpu.esp+=data_size/8;
+    // //edx
+    // opr_dest.type=OPR_MEM;
+    // opr_dest.addr=cpu.esp;
+    // opr_dest.data_size=data_size;
+    // opr_dest.sreg=SREG_DS;
+    // operand_read(&opr_dest);
+    // cpu.edx=opr_dest.val;  
+    // cpu.esp+=data_size/8;
+    // //ecx
+    // opr_dest.type=OPR_MEM;
+    // opr_dest.addr=cpu.esp;
+    // opr_dest.data_size=data_size;
+    // opr_dest.sreg=SREG_DS;
+    // operand_read(&opr_dest);
+    // cpu.ecx=opr_dest.val;  
+    // cpu.esp+=data_size/8;
+    // //eax
+    // opr_dest.type=OPR_MEM;
+    // opr_dest.addr=cpu.esp;
+    // opr_dest.data_size=data_size;
+    // opr_dest.sreg=SREG_DS;
+    // operand_read(&opr_dest);
+    // cpu.eax=opr_dest.val;  
+    // cpu.esp+=data_size/8;
 
-    return 1;
+    // return 1;
 }
