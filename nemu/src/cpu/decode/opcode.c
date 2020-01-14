55dc08cf7570197eb0ea6f5c351789f495299ea7
tracer-pa-public <tracer@njuics.org>
2020-01-15 01:48:33 +0800

    >  clean 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  01:48:33 up 4 days, 19:03,  1 user,  load average: 0.65, 0.62, 0.48 90ce7f367bae0f3a1c3e6f6dfe5842bf6d0d17d0

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -31,7 +31,7 @@ instr_func opcode_entry[256] = {
     /* 0x6c - 0x6f*/ inv, inv, inv, inv,
     /* 0x70 - 0x73*/ inv, inv, jb_short_, jae_short_,
     /* 0x74 - 0x77*/ je_short_, jne_short_, jna_short_, ja_short_,//74je_short_,75:jne_short_,76:jna_short_
-    /* 0x78 - 0x7b*/ js_short_, __ref_jns_short_, inv, inv,
+    /* 0x78 - 0x7b*/ js_short_, jns_short_, inv, inv,
     /* 0x7c - 0x7f*/ jl_short_, jge_short_, jle_short_, jg_short_,//7f:jg_short_,7e:jle_short_,7c:jl_short_,7d:jge_short_
     /* 0x80 - 0x83*/ group_1_b, group_1_v, nemu_trap, group_1_bv,//10.1783group_1_bv
     /* 0x84 - 0x87*/ test_r2rm_b, test_r2rm_v, inv, inv,//85:test_r2rm_v,84:test_r2rm_b
