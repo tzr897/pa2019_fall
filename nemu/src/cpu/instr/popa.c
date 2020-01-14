caf2339bafe343ceb75f2bb075c664ea4c82aab9
tracer-pa-public <tracer@njuics.org>
2020-01-15 00:53:35 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  00:53:35 up 4 days, 18:09,  1 user,  load average: 0.98, 0.41, 0.30 917f470057612143875265d98b091f9b523ad00a

--- nemu/src/cpu/instr/popa.c
+++ nemu/src/cpu/instr/popa.c
@@ -2,6 +2,7 @@
 
 make_instr_func(popa)
 {
+    //edi
     opr_dest.type=OPR_MEM;
     opr_dest.addr=cpu.esp;
     opr_dest.data_size=data_size;
@@ -9,6 +10,56 @@ make_instr_func(popa)
     operand_read(&opr_dest);
     cpu.edi=opr_dest.val;  
     cpu.esp+=data_size/8;
+    //esi
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.data_size=data_size;
+    opr_dest.sreg=SREG_DS;
+    operand_read(&opr_dest);
+    cpu.esi=opr_dest.val;  
+    cpu.esp+=data_size/8;
+    //ebp
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.data_size=data_size;
+    opr_dest.sreg=SREG_DS;
+    operand_read(&opr_dest);
+    cpu.ebp=opr_dest.val;  
+    cpu.esp+=data_size/8;
+    //skip esp
+    cpu.esp+=data_size/8;
+    //ebx
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.data_size=data_size;
+    opr_dest.sreg=SREG_DS;
+    operand_read(&opr_dest);
+    cpu.ebx=opr_dest.val;  
+    cpu.esp+=data_size/8;
+    //edx
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.data_size=data_size;
+    opr_dest.sreg=SREG_DS;
+    operand_read(&opr_dest);
+    cpu.edx=opr_dest.val;  
+    cpu.esp+=data_size/8;
+    //ecx
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.data_size=data_size;
+    opr_dest.sreg=SREG_DS;
+    operand_read(&opr_dest);
+    cpu.ecx=opr_dest.val;  
+    cpu.esp+=data_size/8;
+    //eax
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.data_size=data_size;
+    opr_dest.sreg=SREG_DS;
+    operand_read(&opr_dest);
+    cpu.eax=opr_dest.val;  
+    cpu.esp+=data_size/8;
 
-
+    return 1;
 }
