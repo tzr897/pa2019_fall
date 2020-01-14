1d5babc7259ef46c67b798eb5d4baeec5c55a407
tracer-pa-public <tracer@njuics.org>
2020-01-15 00:59:50 +0800

    >  nemu 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  00:59:50 up 4 days, 18:15,  1 user,  load average: 0.08, 0.27, 0.27 7c69f41d5e33fd543f44a849fdac0c0c2fb9b49b

--- nemu/src/cpu/instr/iret.c
+++ nemu/src/cpu/instr/iret.c
@@ -4,7 +4,6 @@ make_instr_func(iret)
 {
     //Pop EIP
 	OPERAND p_eip;
-	
 	p_eip.data_size=data_size;
 	p_eip.type=OPR_MEM;
 	p_eip.addr=cpu.esp;
@@ -12,5 +11,25 @@ make_instr_func(iret)
     operand_read(&p_eip);
     cpu.esp+=data_size/8;
 	cpu.eip=p_eip.val;
+
+    //Pop CS
+    OPERAND p_cs;
+	p_cs.data_size=data_size;
+	p_cs.type=OPR_MEM;
+	p_cs.addr=cpu.esp;
+	p_cs.sreg=SREG_DS;
+    operand_read(&p_cs);
+    cpu.esp+=data_size/8;
+	cpu.cs.val=p_cs.val;
+    
+    //Pop EFLAGS
+    OPERAND p_eflags;
+	p_eflags.data_size=data_size;
+	p_eflags.type=OPR_MEM;
+	p_eflags.addr=cpu.esp;
+	p_eflags.sreg=SREG_DS;
+    operand_read(&p_eflags);
+    cpu.esp+=data_size/8;
+	cpu.cs.val=p_eflags.val;
 	
 }
