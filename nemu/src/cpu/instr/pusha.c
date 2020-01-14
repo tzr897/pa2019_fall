8b4646b476916dd80e86f8715ea8a3a57dfd186b
monitor <tracer@njuics.org>
2020-01-15 01:05:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/pusha.c
+++ nemu/src/cpu/instr/pusha.c
@@ -2,5 +2,14 @@
 
 make_instr_func(pusha)
 {
-    
+    //temp <- esp
+    uint32_t temp=cpu.esp;
+
+    cpu.esp-=data_size/8;
+    opr_dest.data_size=data_size;
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.sreg=SREG_DS;//
+    opr_dest.val=cpu.eax.val;
+    operand_write(&opr_dest);
 }
