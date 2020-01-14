90af1b791b41fbfcf8f07ffda56f6a55177f562d
monitor <tracer@njuics.org>
2020-01-14 15:12:01 +0800

    git monitor commit.

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,11 +11,25 @@ void raise_intr(uint8_t intr_no)
 // Trigger an exception/interrupt with 'intr_no'
 // 'intr_no' is the index to the IDT
 
+/*
+operand_read(&opr_src);
+
+    cpu.esp-=data_size/8;
+
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    
+    opr_dest.sreg=SREG_DS;//
+    opr_dest.val=opr_src.val;
+    operand_write(&opr_dest);
+ */
 //Push EFLAGS
-OPERAND p_eflags;
-p_eflags.val=cpu.eflags.val;
-p_eflags.data_size=data_size;
-cpu.esp-=4;
+	OPERAND p_eflags;
+	p_eflags.data_size=data_size;
+	cpu.esp-=data_size/8;
+	p_eflags.sreg=SREG_DS;
+	p_eflags.val=cpu.eflags.val;
+	operand_write(&p_eflags);
 
 //Push CS
 
