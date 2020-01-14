733a7cff753c087b6c845023bd5c7919693d7d82
tracer-pa-public <tracer@njuics.org>
2020-01-15 01:36:20 +0800

    >  clean 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  01:36:20 up 4 days, 18:51,  1 user,  load average: 0.21, 0.31, 0.32 6ef82afdf5022f719684fa44813dd14846c5d926

--- nemu/src/cpu/instr/int.c
+++ nemu/src/cpu/instr/int.c
@@ -6,7 +6,7 @@ make_instr_func(int_)
     OPERAND i;
     i.type=OPR_IMM;
     i.addr=eip+1;
-    i.data_size=data_size;
+    i.data_size=8;
     i.sreg=SREG_CS;
     operand_read(&i);
     raise_sw_intr(i.val);
