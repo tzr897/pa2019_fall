1378581724f621148596e6096aaeb547c62281da
tracer-pa-public <tracer@njuics.org>
2020-01-15 00:27:00 +0800

    >  clean 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  00:27:00 up 4 days, 17:42,  1 user,  load average: 0.48, 0.29, 0.20 2cec7a81ddf80966f0145075524d769a40686c3

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -2,5 +2,12 @@
 
 make_instr_func(lidt)
 {
-    
+    int len=1;
+    OPERAND rm;
+    rm.data_size=data_size;
+    len+=modrm_rm(eip + 1, &rm);
+    operand_read(&rm);
+	cpu.idtr.limit = rm.val & 0xffff;
+    cpu.idtr.base = rm.val;
+    return len;
 }
