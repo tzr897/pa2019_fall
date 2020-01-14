ca2ceacefb83c1fbe13fab30eeb17ebcd3bd15a6
monitor <tracer@njuics.org>
2020-01-15 00:52:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/popa.c
+++ nemu/src/cpu/instr/popa.c
@@ -2,5 +2,13 @@
 
 make_instr_func(popa)
 {
-    
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.data_size=data_size;
+    opr_dest.sreg=SREG_DS;
+    operand_read(&opr_dest);
+    cpu.edi=opr_dest.val;  
+    cpu.esp+=data_size/8;
+
+
 }
