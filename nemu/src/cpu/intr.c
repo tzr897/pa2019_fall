d7df929852faa085b6b5b5d6c443e60451aee6ef
tracer-pa-public <tracer@njuics.org>
2020-01-15 15:49:56 +0800

    >  clean 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  15:49:56 up 4 days, 19:21,  1 user,  load average: 0.67, 0.46, 0.38 b728004e403a9789fe01dd284773f5fd45a62182

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -44,7 +44,7 @@ void raise_intr(uint8_t intr_no)
 
 // Find the IDT entry using 'intr_no'
 	GateDesc gd;
-	gd.val[0]=laddr_read((cpu.idtr.base+8*intr_no),8);
+	gd.val[0]=laddr_read((cpu.idtr.base+8*intr_no),4);
 	gd.val[1]=laddr_read((cpu.idtr.base+8*intr_no+4),4);
 
 
