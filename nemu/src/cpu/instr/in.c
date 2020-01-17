232ffdb9dff955926cace39ea950fa976b4eefd5
monitor <tracer@njuics.org>
2020-01-18 00:21:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -2,7 +2,22 @@
 
 make_instr_func(in_b)
 {
-    return 0;
+    OPERAND from,to;
+
+    from.addr=REG_DX;
+    from.type=OPR_REG;
+    from.data_size=16;
+
+    to.addr=REG_AL;
+    to.type=OPR_REG;
+    to.data_size=8;
+
+    operand_read(&from);
+    to.val=pio_read(from.val, 1);
+    operand_write(&to);
+    
+    return 1;
+
 }
 
 make_instr_func(in_v)
@@ -10,5 +25,13 @@ make_instr_func(in_v)
     return 0;
 }
 
-//void pio_write(uint16_t port, size_t len, uint32_t data)
-//uint32_t pio_read(uint16_t port, size_t len)
+//void pio_write(uint16_t port, size_t len, uint32_t data);
+//uint32_t pio_read(uint16_t port, size_t len);
+
+// opr_dest.type=OPR_MEM;
+//     opr_dest.addr=cpu.esp;
+//     opr_dest.data_size=data_size;
+//     opr_dest.sreg=SREG_DS;
+//     operand_read(&opr_dest);
+//     cpu.edi=opr_dest.val;  
+//     cpu.esp+=data_size/8;
