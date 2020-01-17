2c1e80712287e0d9786189ffd6ed624e5efba380
monitor <tracer@njuics.org>
2020-01-18 00:12:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -2,10 +2,13 @@
 
 make_instr_func(out_b)
 {
-    return 0(
+    
 }
 
 make_instr_func(out_v)
 {
     return 0;
 }
+
+//void pio_write(uint16_t port, size_t len, uint32_t data)
+//uint32_t pio_read(uint16_t port, size_t len)
