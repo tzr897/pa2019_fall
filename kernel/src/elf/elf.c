48a7e84c80a9862fc3b8c2dd9daef4196f1e2d89
tracer-pa-public <tracer@njuics.org>
2020-01-18 22:42:10 +0800

    >  clean 171870667 tzr99 Linux debian 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  22:42:10 up 5 days,  3:09,  1 user,  load average: 0.96, 0.72, 0.45 6d5bc094ea4f2150f34171c2b3d0a9c91a1a3f65

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -58,14 +58,14 @@ uint32_t loader()//renwu:shixian loader()
 
 			//12.8 begin
 			uint32_t p_a=mm_malloc(ph->p_vaddr, ph->p_memsz);
-			memcpy((void*)p_a, (void*)ph->p_offset, ph->p_filesz);
+			//memcpy((void*)p_a, (void*)ph->p_offset, ph->p_filesz);
 
 
 			//12.8 end
 
 			//pa4-2.3.2 begin 
 			//ide_read((uint8_t*)p_a, (uint32_t)(ph->p_offset+ELF_OFFSET_IN_DISK), (uint32_t)ph->p_filesz);
-			//ide_write((uint8_t*)p_a, (uint32_t)(elf ), 4096);
+			ide_write((uint8_t*)p_a, (uint32_t)(ph->p_offset+ELF_OFFSET_IN_DISK), 4096);
 			//pa4-2.3.2 end
 
 #ifdef IA32_PAGE
